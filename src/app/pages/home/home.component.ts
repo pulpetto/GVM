e728a624cc83d5510e2a1201a7d49f966f49d965
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-09 10:34:52 +0200

    home page initial design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+    words = ['partner', 'tracker', 'buddy', 'assistant', 'coach', 'instructor'];
+    activeWordIndex: number = 0;
+    // eslint-disable-next-line
+    interval!: any;
+
+    @ViewChild('myElement') myElement!: ElementRef;
+
+    ngAfterViewInit() {
+        const threshold = 0.2;
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        this.interval = setInterval(() => {
+                            if (
+                                this.activeWordIndex + 1 ===
+                                this.words.length
+                            ) {
+                                this.activeWordIndex = 0;
+                            } else {
+                                this.activeWordIndex++;
+                            }
+                        }, 2000);
+                    } else {
+                        clearInterval(this.interval);
+                    }
+                });
+            },
+            { threshold }
+        );
+
+        observer.observe(this.myElement.nativeElement);
+    }
+}
