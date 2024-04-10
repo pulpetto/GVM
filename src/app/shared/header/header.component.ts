45a5dc2a7c54bad729b1244bbbd58c660456ada9
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-10 05:57:17 +0200

    add hamburger & menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+    isMenuVisible: boolean = false;
 }
