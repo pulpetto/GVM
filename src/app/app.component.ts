fcece534fbbf14f59e1152b710f953a10db19af7
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-10 05:56:55 +0200

    import component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, HostBinding, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent],
+    imports: [RouterOutlet, HomeComponent, HeaderComponent],
 })
 export class AppComponent {
     title = 'GVM';
