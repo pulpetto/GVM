78ae3262f626a580dbb3e75da80d7027046c9cbb
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-14 14:25:51 +0200

    configure routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./pages/login/login.component').then(
+                (component) => component.LoginComponent
+            ),
+    },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', pathMatch: 'full', component: NotFoundComponent },
+];
