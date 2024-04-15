b874a7065a6c5cc68676b41b86915e55abb075cb
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-15 07:38:57 +0200

    integrate dark mode with local storage

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,10 +1,25 @@
-import { HostBinding, Injectable, signal } from '@angular/core';
+import { effect, HostBinding, Injectable, signal } from '@angular/core';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ThemeService {
-    darkMode = signal<boolean>(false);
+    darkMode = signal<boolean>(
+        typeof window !== 'undefined'
+            ? JSON.parse(window.localStorage.getItem('darkMode') ?? 'false')
+            : true
+    );
+
+    constructor() {
+        effect(() => {
+            if (typeof window !== 'undefined') {
+                window.localStorage.setItem(
+                    'darkMode',
+                    JSON.stringify(this.darkMode())
+                );
+            }
+        });
+    }
 
     @HostBinding('class.dark') get mode() {
         return this.darkMode();
