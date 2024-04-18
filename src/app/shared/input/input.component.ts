f7230e417635cca9397c451a3384fa5b5e000c4d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-18 05:44:00 +0200

    fix focusing

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
@@ -16,8 +16,15 @@ export class InputComponent implements OnInit {
     @Input() inputMask!: string;
     id!: string;
 
+    @ViewChild('inputElement') inputRef!: ElementRef;
+
     ngOnInit() {
         // prettier-ignore
         this.id = new Array(5).join().replace(/(.|$)/g, function(){return ((Math.random()*36)|0).toString(36)[Math.random()<.5?"toString":"toUpperCase"]();});
+    }
+
+    focusOnInput() {
+        console.log('h');
+        this.inputRef.nativeElement.focus();
     }
 }
