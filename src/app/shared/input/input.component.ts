4f5de695eee0f75a22265aa9f39c319f76d183be
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-18 04:56:21 +0200

    add input component's logic & template

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-input',
-  standalone: true,
-  imports: [],
-  templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+    selector: 'app-input',
+    standalone: true,
+    imports: [],
+    templateUrl: './input.component.html',
+    styleUrl: './input.component.css',
 })
-export class InputComponent {
+export class InputComponent implements OnInit {
+    @Input({ required: true }) type!: string;
+    @Input({ required: true }) label!: string;
+    id!: string;
 
+    ngOnInit() {
+        // prettier-ignore
+        this.id = new Array(5).join().replace(/(.|$)/g, function(){return ((Math.random()*36)|0).toString(36)[Math.random()<.5?"toString":"toUpperCase"]();});
+    }
 }
