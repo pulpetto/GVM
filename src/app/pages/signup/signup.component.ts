b313f42a0817cdb93810f900a12ef84482696724
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-19 05:33:50 +0200

    add form builder

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InputComponent } from '../../shared/input/input.component';
+import {
+    FormBuilder,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 
 @Component({
     selector: 'app-signup',
     standalone: true,
     templateUrl: './signup.component.html',
     styleUrl: './signup.component.css',
-    imports: [InputComponent],
+    imports: [InputComponent, ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent implements OnInit {
+    form!: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            username: ['', Validators.required],
+            email: ['', Validators.required, Validators.email],
+            password: ['', Validators.required],
+            repeatPassword: ['', Validators.required],
+        });
+    }
+}
