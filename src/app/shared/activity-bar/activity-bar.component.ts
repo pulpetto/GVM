d86db6b81f0924a36484d9132666f6f0f0bbd6d8
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-25 12:08:18 +0200

    clear component

--- src/app/shared/activity-bar/activity-bar.component.ts
+++ src/app/shared/activity-bar/activity-bar.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-activity-bar',
     standalone: true,
-    imports: [RouterLink],
+    imports: [],
     templateUrl: './activity-bar.component.html',
     styleUrl: './activity-bar.component.css',
 })
