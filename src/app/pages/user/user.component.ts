c8dbaed0311a67784244ad6e2d3e127c395ad036
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-04-25 09:07:56 +0200

    add initial markup

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ActivityBarComponent } from '../../shared/activity-bar/activity-bar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css',
+    imports: [ActivityBarComponent, RouterOutlet],
 })
-export class UserComponent {
-
-}
+export class UserComponent {}
