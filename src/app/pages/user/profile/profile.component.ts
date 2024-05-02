efdc393a365ecfcf2a94d9d390c775fc903056e9
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-02 10:12:44 +0200

    swap old code for reusable component

--- src/app/pages/user/profile/profile.component.ts
+++ src/app/pages/user/profile/profile.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { WorkoutPreviewComponent } from '../../../shared/workout-preview/workout-preview.component';
 import { CommentComponent } from '../../../shared/comment/comment.component';
+import { FollowNotificationComponent } from '../../../shared/follow-notification/follow-notification.component';
 
 @Component({
     selector: 'app-profile',
     standalone: true,
     templateUrl: './profile.component.html',
     styleUrl: './profile.component.css',
-    imports: [WorkoutPreviewComponent, CommentComponent],
+    imports: [
+        WorkoutPreviewComponent,
+        CommentComponent,
+        FollowNotificationComponent,
+    ],
 })
 export class ProfileComponent {}
