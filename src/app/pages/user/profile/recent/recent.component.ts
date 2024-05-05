071ed6384fdbf5ce89ff187062e12c38d1c6e93a
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-05 13:29:38 +0200

    add code

--- src/app/pages/user/profile/recent/recent.component.ts
+++ src/app/pages/user/profile/recent/recent.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { OneOptionModalComponent } from '../../../../shared/one-option-modal/one-option-modal.component';
+import { WorkoutPreviewComponent } from '../../../../shared/workout-preview/workout-preview.component';
+import { FollowNotificationComponent } from '../../../../shared/follow-notification/follow-notification.component';
+import { CommentComponent } from '../../../../shared/comment/comment.component';
 
 @Component({
-  selector: 'app-recent',
-  standalone: true,
-  imports: [],
-  templateUrl: './recent.component.html',
-  styleUrl: './recent.component.css'
+    selector: 'app-recent',
+    standalone: true,
+    templateUrl: './recent.component.html',
+    styleUrl: './recent.component.css',
+    imports: [
+        OneOptionModalComponent,
+        WorkoutPreviewComponent,
+        FollowNotificationComponent,
+        CommentComponent,
+    ],
 })
 export class RecentComponent {
+    modalVisibility: boolean = false;
 
+    tabChange() {}
 }
