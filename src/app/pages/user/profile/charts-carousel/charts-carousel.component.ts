589ddaf27cfa1e13068329c5d4dc74f0698a2c5e
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-07 05:19:52 +0200

    setup base structure

--- src/app/pages/user/profile/charts-carousel/charts-carousel.component.ts
+++ src/app/pages/user/profile/charts-carousel/charts-carousel.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { OneOptionModalComponent } from '../../../../shared/one-option-modal/one-option-modal.component';
+import { CarouselComponent } from '../../../../shared/carousel/carousel.component';
+import { ButtonForModalComponent } from '../../../../shared/button-for-modal/button-for-modal.component';
 
 @Component({
-  selector: 'app-charts-carousel',
-  standalone: true,
-  imports: [],
-  templateUrl: './charts-carousel.component.html',
-  styleUrl: './charts-carousel.component.css'
+    selector: 'app-charts-carousel',
+    standalone: true,
+    templateUrl: './charts-carousel.component.html',
+    styleUrl: './charts-carousel.component.css',
+    imports: [
+        OneOptionModalComponent,
+        CarouselComponent,
+        ButtonForModalComponent,
+    ],
 })
 export class ChartsCarouselComponent {
-
+    handle() {}
 }
