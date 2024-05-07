57e4c506e4c183394ffe0a0956808c66fc73378b
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-07 09:28:47 +0200

    make component reusable

--- src/app/shared/workoutViews/workout-mini-preview/workout-mini-preview.component.ts
+++ src/app/shared/workoutViews/workout-mini-preview/workout-mini-preview.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-workout-mini-preview',
     standalone: true,
-    imports: [],
+    imports: [RouterModule],
     templateUrl: './workout-mini-preview.component.html',
     styleUrl: './workout-mini-preview.component.css',
 })
-export class WorkoutMiniPreviewComponent {}
+export class WorkoutMiniPreviewComponent {
+    @Input({ required: true }) name!: string;
+    @Input({ required: true }) exercises!: string[];
+    @Input({ required: true }) muscleGroups!: string[];
+    @Input({ required: true }) equipment!: string[];
+
+    get routeName(): string {
+        return this.name.replace(/\s&\s/g, '&');
+    }
+}
