aa0fa472dca11655522903a0b39da758f933e118
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-08 19:07:33 +0200

    add import

--- src/app/shared/workoutViews/workout-mini-preview/workout-mini-preview.component.ts
+++ src/app/shared/workoutViews/workout-mini-preview/workout-mini-preview.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonForModalComponent } from '../../button-for-modal/button-for-modal.component';
 
 @Component({
     selector: 'app-workout-mini-preview',
     standalone: true,
-    imports: [RouterModule],
     templateUrl: './workout-mini-preview.component.html',
     styleUrl: './workout-mini-preview.component.css',
+    imports: [RouterModule, ButtonForModalComponent],
 })
 export class WorkoutMiniPreviewComponent {
     @Input({ required: true }) name!: string;
