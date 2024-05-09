3ee82aa4a44b9809d1c46108570f14b577e245d6
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-09 05:38:42 +0200

    add heading

--- src/app/shared/workoutViews/workout-full-view/workout-full-view.component.ts
+++ src/app/shared/workoutViews/workout-full-view/workout-full-view.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-workout-full-view',
-  standalone: true,
-  imports: [],
-  templateUrl: './workout-full-view.component.html',
-  styleUrl: './workout-full-view.component.css'
+    selector: 'app-workout-full-view',
+    standalone: true,
+    imports: [],
+    templateUrl: './workout-full-view.component.html',
+    styleUrl: './workout-full-view.component.css',
 })
-export class WorkoutFullViewComponent {
-
-}
+export class WorkoutFullViewComponent {}
