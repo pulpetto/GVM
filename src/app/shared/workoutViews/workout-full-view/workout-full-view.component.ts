155b80ed02be885517940eb3449a79684c2be9e7
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-09 11:46:10 +0200

    add exercise markup

--- src/app/shared/workoutViews/workout-full-view/workout-full-view.component.ts
+++ src/app/shared/workoutViews/workout-full-view/workout-full-view.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ChartsCarouselComponent } from '../../charts-carousel/charts-carousel.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-workout-full-view',
     standalone: true,
     templateUrl: './workout-full-view.component.html',
     styleUrl: './workout-full-view.component.css',
-    imports: [ChartsCarouselComponent],
+    imports: [ChartsCarouselComponent, CommonModule],
 })
 export class WorkoutFullViewComponent {}
