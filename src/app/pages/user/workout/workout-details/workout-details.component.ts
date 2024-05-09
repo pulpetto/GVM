7b726c5415421327a2e96d45f7e3d0606c7b7189
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-09 05:25:13 +0200

    display component

--- src/app/pages/user/workout/workout-details/workout-details.component.ts
+++ src/app/pages/user/workout/workout-details/workout-details.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { WorkoutFullViewComponent } from '../../../../shared/workoutViews/workout-full-view/workout-full-view.component';
 
 @Component({
     selector: 'app-workout-details',
     standalone: true,
-    imports: [],
     templateUrl: './workout-details.component.html',
     styleUrl: './workout-details.component.css',
+    imports: [WorkoutFullViewComponent],
 })
 export class WorkoutDetailsComponent {}
