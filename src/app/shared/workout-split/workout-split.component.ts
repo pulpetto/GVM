dc37180b697866116d12e92ea86f7455a99cfc98
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-10 13:06:24 +0200

    animate height when opening/closing

--- src/app/shared/workout-split/workout-split.component.ts
+++ src/app/shared/workout-split/workout-split.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 import { WorkoutMiniPreviewComponent } from '../workoutViews/workout-mini-preview/workout-mini-preview.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-workout-split',
     standalone: true,
     templateUrl: './workout-split.component.html',
     styleUrl: './workout-split.component.css',
-    imports: [WorkoutMiniPreviewComponent],
+    imports: [WorkoutMiniPreviewComponent, CommonModule],
 })
-export class WorkoutSplitComponent {}
+export class WorkoutSplitComponent {
+    isOpen: boolean = false;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    calculateHeight(element: any) {
+        element.style.height =
+            Array.prototype.reduce.call(
+                element.childNodes,
+                function (p, c) {
+                    return p + (c.offsetHeight || 0);
+                },
+                0
+            ) + 'px';
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    toggleExpand(element: any) {
+        if (!element.style.height || element.style.height == '0px') {
+            this.calculateHeight(element);
+        } else {
+            element.style.height = '0px';
+        }
+    }
+}
