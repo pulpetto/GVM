1ac8c24e4247fbc8844f9f5f30b314e5abe42346
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-10 12:23:53 +0200

    refactor code into separate component

--- src/app/pages/user/workout/workout.component.ts
+++ src/app/pages/user/workout/workout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { WorkoutMiniPreviewComponent } from '../../../shared/workoutViews/workout-mini-preview/workout-mini-preview.component';
+import { WorkoutSplitComponent } from '../../../shared/workout-split/workout-split.component';
 
 @Component({
     selector: 'app-workout',
     standalone: true,
     templateUrl: './workout.component.html',
     styleUrl: './workout.component.css',
-    imports: [WorkoutMiniPreviewComponent],
+    imports: [WorkoutMiniPreviewComponent, WorkoutSplitComponent],
 })
 export class WorkoutComponent {}
--- src/app/shared/workout-split/workout-split.component.ts
+++ src/app/shared/workout-split/workout-split.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { WorkoutMiniPreviewComponent } from '../workoutViews/workout-mini-preview/workout-mini-preview.component';
 
 @Component({
     selector: 'app-workout-split',
     standalone: true,
-    imports: [],
     templateUrl: './workout-split.component.html',
     styleUrl: './workout-split.component.css',
+    imports: [WorkoutMiniPreviewComponent],
 })
 export class WorkoutSplitComponent {}
