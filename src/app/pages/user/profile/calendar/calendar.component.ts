0f6e57cf9221394968dc4b285ec8e78e8e9b95f8
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-11 20:01:28 +0200

    add dropdown menu

--- src/app/pages/user/profile/calendar/calendar.component.ts
+++ src/app/pages/user/profile/calendar/calendar.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-calendar',
-  standalone: true,
-  imports: [],
-  templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.css'
+    selector: 'app-calendar',
+    standalone: true,
+    imports: [],
+    templateUrl: './calendar.component.html',
+    styleUrl: './calendar.component.css',
 })
 export class CalendarComponent {
+    isOpen: boolean = false;
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    calculateHeight(element: any) {
+        element.style.height =
+            Array.prototype.reduce.call(
+                element.childNodes,
+                function (p, c) {
+                    return p + (c.offsetHeight || 0);
+                },
+                0
+            ) + 'px';
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    toggleExpand(element: any) {
+        if (!element.style.height || element.style.height == '0px') {
+            this.calculateHeight(element);
+        } else {
+            element.style.height = '0px';
+        }
+    }
 }
