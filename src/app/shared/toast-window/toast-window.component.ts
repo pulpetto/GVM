9cfaeab8ad992caa227904c9e13c4a344b1ddec4
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-23 13:46:15 +0200

    add code for functionality

--- src/app/shared/toast-window/toast-window.component.ts
+++ src/app/shared/toast-window/toast-window.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-toast-window',
-  standalone: true,
-  imports: [],
-  templateUrl: './toast-window.component.html',
-  styleUrl: './toast-window.component.css'
+    selector: 'app-toast-window',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './toast-window.component.html',
+    styleUrl: './toast-window.component.css',
 })
 export class ToastWindowComponent {
+    @Input({ required: true }) message!: string;
+    @Input({ required: true }) errorMessage: boolean = false;
+    @Input({ required: true }) visibility: boolean = false;
 
+    @Output() closeEvent = new EventEmitter<void>();
 }
