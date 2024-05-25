871d38f934c28229a5fbada63252344ae2faaf81
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-25 08:50:26 +0200

    add input link

--- src/app/shared/icon-button/icon-button.component.ts
+++ src/app/shared/icon-button/icon-button.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-icon-button',
     standalone: true,
-    imports: [],
+    imports: [RouterModule],
     templateUrl: './icon-button.component.html',
     styleUrl: './icon-button.component.css',
 })
 export class IconButtonComponent {
-    @Input() name!: string;
+    @Input({ required: true }) name!: string;
+    @Input({ required: true }) link!: string;
 }
