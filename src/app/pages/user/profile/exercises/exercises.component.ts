fe9ec4d48a477bd6cfa8343c40325d96df990475
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-25 10:55:40 +0200

    add data

--- src/app/pages/user/profile/exercises/exercises.component.ts
+++ src/app/pages/user/profile/exercises/exercises.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../../../../shared/input/input.component';
+import { MuscleGroup } from '../../../../interfaces/muscle-group';
 
 @Component({
     selector: 'app-exercises',
@@ -8,4 +9,23 @@ import { InputComponent } from '../../../../shared/input/input.component';
     styleUrl: './exercises.component.css',
     imports: [InputComponent],
 })
-export class ExercisesComponent {}
+export class ExercisesComponent {
+    muscleGroups: MuscleGroup[] = [
+        { name: 'Upper Back', imageUrl: '' },
+        { name: 'Lower Back', imageUrl: '' },
+        { name: 'Lats', imageUrl: '' },
+        { name: 'Chest', imageUrl: '' },
+        { name: 'Abs', imageUrl: '' },
+        { name: 'Biceps', imageUrl: '' },
+        { name: 'Triceps', imageUrl: '' },
+        { name: 'Rear Delts', imageUrl: '' },
+        { name: 'Side Delts', imageUrl: '' },
+        { name: 'Front Delts', imageUrl: '' },
+        { name: 'Forearms', imageUrl: '' },
+        { name: 'Traps', imageUrl: '' },
+        { name: 'Neck', imageUrl: '' },
+        { name: 'Quads', imageUrl: '' },
+        { name: 'Hamstrings', imageUrl: '' },
+        { name: 'Glutes', imageUrl: '' },
+        { name: 'Calves', imageUrl: '' },
+    ];
