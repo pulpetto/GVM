171c1e39d1ab58bcb9b1e03e5b9304db24d7107c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-29 13:25:50 +0200

    add component

---
+++ src/app/shared/tabs/tabs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-tabs',
+    standalone: true,
+    imports: [],
+    templateUrl: './tabs.component.html',
+    styleUrl: './tabs.component.css',
+})
+export class TabsComponent {}
