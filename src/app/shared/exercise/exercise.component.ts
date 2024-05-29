13123d1dacc7ebc6d1dad52b2b2ea7ea50694144
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-29 13:30:03 +0200

    add initial structure

--- src/app/shared/exercise/exercise.component.ts
+++ src/app/shared/exercise/exercise.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ChartsCarouselComponent } from '../charts-carousel/charts-carousel.component';
+import { TabsComponent } from '../tabs/tabs.component';
 
 @Component({
-  selector: 'app-exercise',
-  standalone: true,
-  imports: [],
-  templateUrl: './exercise.component.html',
-  styleUrl: './exercise.component.css'
+    selector: 'app-exercise',
+    standalone: true,
+    templateUrl: './exercise.component.html',
+    styleUrl: './exercise.component.css',
+    imports: [ChartsCarouselComponent, TabsComponent],
 })
-export class ExerciseComponent {
-
-}
+export class ExerciseComponent {}
