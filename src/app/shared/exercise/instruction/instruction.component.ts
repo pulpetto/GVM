637a350b60e21e2a0cba4752c5ad7ee914f79e45
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-30 10:06:52 +0200

    add component

---
+++ src/app/shared/exercise/instruction/instruction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-instruction',
+    standalone: true,
+    imports: [],
+    templateUrl: './instruction.component.html',
+    styleUrl: './instruction.component.css',
+})
+export class InstructionComponent {}
