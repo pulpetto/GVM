0f39e8efb6e194054c79b2abb2547a402c0e4249
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-30 07:48:08 +0200

    animate tab change

--- src/app/shared/tabs/tabs.component.ts
+++ src/app/shared/tabs/tabs.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    ElementRef,
+    inject,
+    QueryList,
+    ViewChildren,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-tabs',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './tabs.component.html',
     styleUrl: './tabs.component.css',
 })
-export class TabsComponent {}
+export class TabsComponent implements AfterViewInit {
+    @ViewChildren('tabButton') tabButtons!: QueryList<ElementRef>;
+
+    cdr = inject(ChangeDetectorRef);
+
+    activeTabIndex: number = 0;
+    underlineWidthPx!: number;
+    leftDistancePx: number = 0;
+
+    tabChange($index: number) {
+        this.activeTabIndex = $index;
+
+        this.underlineWidthPx =
+            this.tabButtons.toArray()[$index].nativeElement.clientWidth;
+
+        this.leftDistancePx =
+            this.tabButtons.toArray()[$index].nativeElement.offsetLeft;
+    }
+
+    ngAfterViewInit() {
+        this.underlineWidthPx =
+            this.tabButtons.toArray()[0].nativeElement.clientWidth;
+
+        this.cdr.detectChanges();
+    }
+}
