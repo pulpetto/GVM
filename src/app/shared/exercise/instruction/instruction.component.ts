b3f6b5addc2da6580d6ae9b8278a5892fb5fd81a
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-30 11:50:55 +0200

    add all functionality & markup

--- src/app/shared/exercise/instruction/instruction.component.ts
+++ src/app/shared/exercise/instruction/instruction.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { InstructionStep } from '../../../interfaces/instruction-step';
 
 @Component({
     selector: 'app-instruction',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
     templateUrl: './instruction.component.html',
     styleUrl: './instruction.component.css',
 })
-export class InstructionComponent {}
+export class InstructionComponent {
+    @Input({ required: true }) stepsSimplified!: InstructionStep[];
+    @Input({ required: true }) stepsDetailed!: InstructionStep[];
+
+    simplifiedVersion: boolean = true;
+}
