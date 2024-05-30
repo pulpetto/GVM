59771893930241651deae5b9770b47e089547844
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-05-30 10:01:59 +0200

    add tabs content changing

--- src/app/shared/tabs/tabs.component.ts
+++ src/app/shared/tabs/tabs.component.ts
@@ -2,10 +2,12 @@ import {
     AfterViewInit,
     ChangeDetectorRef,
     Component,
+    ContentChildren,
     ElementRef,
     inject,
     Input,
     QueryList,
+    TemplateRef,
     ViewChildren,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -19,6 +21,10 @@ import { CommonModule } from '@angular/common';
 })
 export class TabsComponent implements AfterViewInit {
     @Input({ required: true }) tabNames!: string[];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    @ContentChildren(TemplateRef) tabContents!: QueryList<TemplateRef<any>>;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    activeTabContent!: TemplateRef<any>;
 
     @ViewChildren('tabButton') tabButtons!: QueryList<ElementRef>;
 
@@ -36,11 +42,15 @@ export class TabsComponent implements AfterViewInit {
 
         this.leftDistancePx =
             this.tabButtons.toArray()[$index].nativeElement.offsetLeft;
+
+        this.activeTabContent = this.tabContents.toArray()[$index];
     }
 
     ngAfterViewInit() {
         this.underlineWidthPx =
             this.tabButtons.toArray()[0].nativeElement.clientWidth;
+
+        this.activeTabContent = this.tabContents.toArray()[0];
 
         this.cdr.detectChanges();
     }
