c3341093bdf1b99f783bfdf6a2792b4ab6409d6c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-05 12:50:41 +0200

    change naming & configure routing

--- src/app/pages/user/workout/workout.component.ts
+++ src/app/pages/user/workout/workout.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { WorkoutMiniPreviewComponent } from '../../../shared/workoutViews/workout-mini-preview/workout-mini-preview.component';
 import { WorkoutSplitComponent } from '../../../shared/workout-split/workout-split.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-workout',
     standalone: true,
     templateUrl: './workout.component.html',
     styleUrl: './workout.component.css',
-    imports: [WorkoutMiniPreviewComponent, WorkoutSplitComponent],
+    imports: [WorkoutMiniPreviewComponent, WorkoutSplitComponent, RouterModule],
 })
 export class WorkoutComponent {}
