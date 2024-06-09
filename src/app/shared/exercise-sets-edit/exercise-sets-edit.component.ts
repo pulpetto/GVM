f9c082ebf73130e654cd8d19c9f4e862655776fd
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-09 20:12:46 +0200

    add component

---
+++ src/app/shared/exercise-sets-edit/exercise-sets-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-exercise-sets-edit',
+    standalone: true,
+    imports: [],
+    templateUrl: './exercise-sets-edit.component.html',
+    styleUrl: './exercise-sets-edit.component.css',
+})
+export class ExerciseSetsEditComponent {}
