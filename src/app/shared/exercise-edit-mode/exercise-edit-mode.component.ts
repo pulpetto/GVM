83442ef31cfba660720b6e3fad0e2a3a2d89a999
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-11 12:35:57 +0200

    add mockup data

--- src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
+++ src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Set } from '../../interfaces/set';
 
 @Component({
     selector: 'app-exercise-edit-mode',
@@ -9,5 +10,12 @@ import { CommonModule } from '@angular/common';
     styleUrl: './exercise-edit-mode.component.css',
 })
 export class ExerciseEditModeComponent {
-    optionsModalVisibility: boolean = true;
+    optionsModalVisibility: boolean = false;
+
+    sets: Set[] = [
+        { number: 1, type: 'normal', weight: 100, reps: 20, rpe: 9, rir: 1 },
+        { number: 2, type: 'normal', weight: 100, reps: 20, rpe: 9, rir: 1 },
+        { number: 3, type: 'normal', weight: 100, reps: 20, rpe: 9, rir: 1 },
+        { number: 4, type: 'normal', weight: 100, reps: 20, rpe: 9, rir: 1 },
+    ];
 }
