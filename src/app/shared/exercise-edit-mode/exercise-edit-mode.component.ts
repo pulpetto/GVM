d38b99b178f6b9a52432decbcd26a23e33ca1fbe
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-12 11:22:34 +0200

    add set types modal

--- src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
+++ src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
@@ -12,6 +12,7 @@ import { InfoModalButtonComponent } from '../info-modal-button/info-modal-button
 })
 export class ExerciseEditModeComponent {
     optionsModalVisibility: boolean = false;
+    setTypeModalVisibility: boolean = false;
 
     sets: Set[] = [
         { number: 1, type: 'W', weight: 100, reps: 20, rpe: 9 },
@@ -19,4 +20,66 @@ export class ExerciseEditModeComponent {
         { number: 3, type: 'W', weight: 100, reps: 20, rpe: 9 },
         { number: 4, type: 'W', weight: 100, reps: 20, rpe: 9 },
     ];
+
+    setTypes = [
+        {
+            name: 'normal',
+            textColor: 'text-white',
+            borderColor: 'border-white',
+            isSelected: true,
+        },
+        {
+            name: 'warmup',
+            textColor: 'text-yellow-500',
+            borderColor: 'border-yellow-500',
+            isSelected: false,
+        },
+        {
+            name: 'failure',
+            textColor: 'text-red-500',
+            borderColor: 'border-red-500',
+            isSelected: false,
+        },
+        {
+            name: 'paused',
+            textColor: 'text-blue-500',
+            borderColor: 'border-blue-500',
+            isSelected: false,
+        },
+        {
+            name: 'drop',
+            textColor: 'text-orange-500',
+            borderColor: 'border-orange-500',
+            isSelected: false,
+        },
+        {
+            name: 'cluster',
+            textColor: 'text-amber-500',
+            borderColor: 'border-amber-500',
+            isSelected: false,
+        },
+        {
+            name: 'iso',
+            textColor: 'text-indigo-500',
+            borderColor: 'border-indigo-500',
+            isSelected: false,
+        },
+        {
+            name: 'partials',
+            textColor: 'text-fuchsia-500',
+            borderColor: 'border-fuchsia-500',
+            isSelected: false,
+        },
+        {
+            name: 'slow eccentric',
+            textColor: 'text-green-500',
+            borderColor: 'border-green-500',
+            isSelected: false,
+        },
+    ];
+
+    onSetTypeChange($index: number) {
+        this.setTypes.forEach((set) => (set.isSelected = false));
+        this.setTypes[$index].isSelected = true;
+    }
 }
