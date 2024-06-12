2c9237217919f05e46a0e261358650198250f27b
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-12 19:46:28 +0200

    refactor code into separate component

--- src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
+++ src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
@@ -1,85 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Set } from '../../interfaces/set';
 import { InfoModalButtonComponent } from '../info-modal-button/info-modal-button.component';
+import { SetComponent } from './set/set.component';
 
 @Component({
     selector: 'app-exercise-edit-mode',
     standalone: true,
     templateUrl: './exercise-edit-mode.component.html',
     styleUrl: './exercise-edit-mode.component.css',
-    imports: [CommonModule, InfoModalButtonComponent],
+    imports: [CommonModule, InfoModalButtonComponent, SetComponent],
 })
 export class ExerciseEditModeComponent {
     optionsModalVisibility: boolean = false;
-    setTypeModalVisibility: boolean = false;
 
-    sets: Set[] = [
-        { number: 1, type: 'W', weight: 100, reps: 20, rpe: 9 },
-        { number: 2, type: 'W', weight: 100, reps: 20, rpe: 9 },
-        { number: 3, type: 'W', weight: 100, reps: 20, rpe: 9 },
-        { number: 4, type: 'W', weight: 100, reps: 20, rpe: 9 },
+    sets = [
+        {
+            number: 1,
+            weight: 100,
+            reps: 20,
+            rpe: 9,
+        },
+        { number: 2, weight: 100, reps: 20, rpe: 9 },
+        { number: 3, weight: 100, reps: 20, rpe: 9 },
+        { number: 4, weight: 100, reps: 20, rpe: 9 },
     ];
-
-    setTypes = [
-        {
-            name: 'normal',
-            textColor: 'text-white',
-            borderColor: 'border-white',
-            isSelected: true,
-        },
-        {
-            name: 'warmup',
-            textColor: 'text-yellow-500',
-            borderColor: 'border-yellow-500',
-            isSelected: false,
-        },
-        {
-            name: 'failure',
-            textColor: 'text-red-500',
-            borderColor: 'border-red-500',
-            isSelected: false,
-        },
-        {
-            name: 'paused',
-            textColor: 'text-blue-500',
-            borderColor: 'border-blue-500',
-            isSelected: false,
-        },
-        {
-            name: 'drop',
-            textColor: 'text-orange-500',
-            borderColor: 'border-orange-500',
-            isSelected: false,
-        },
-        {
-            name: 'cluster',
-            textColor: 'text-amber-500',
-            borderColor: 'border-amber-500',
-            isSelected: false,
-        },
-        {
-            name: 'iso',
-            textColor: 'text-indigo-500',
-            borderColor: 'border-indigo-500',
-            isSelected: false,
-        },
-        {
-            name: 'partials',
-            textColor: 'text-fuchsia-500',
-            borderColor: 'border-fuchsia-500',
-            isSelected: false,
-        },
-        {
-            name: 'slow eccentric',
-            textColor: 'text-green-500',
-            borderColor: 'border-green-500',
-            isSelected: false,
-        },
-    ];
-
-    onSetTypeChange($index: number) {
-        this.setTypes.forEach((set) => (set.isSelected = false));
-        this.setTypes[$index].isSelected = true;
-    }
 }
--- src/app/shared/exercise-edit-mode/set/set.component.ts
+++ src/app/shared/exercise-edit-mode/set/set.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-set',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './set.component.html',
     styleUrl: './set.component.css',
 })
-export class SetComponent {}
+export class SetComponent {
+    @Input({ required: true }) lighterBg: boolean = false;
+    @Input({ required: true }) number!: number;
+    setTypeIndex: number = 0;
+    @Input({ required: true }) weight!: number;
+    @Input({ required: true }) reps!: number;
+    @Input({ required: true }) rpe!: number;
+
+    modalVisibility: boolean = false;
+
+    setTypes = [
+        {
+            name: 'normal',
+            textColor: 'text-white',
+            borderColor: 'border-white',
+            isSelected: true,
+        },
+        {
+            name: 'warmup',
+            textColor: 'text-yellow-500',
+            borderColor: 'border-yellow-500',
+            isSelected: false,
+        },
+        {
+            name: 'failure',
+            textColor: 'text-red-500',
+            borderColor: 'border-red-500',
+            isSelected: false,
+        },
+        {
+            name: 'paused',
+            textColor: 'text-blue-500',
+            borderColor: 'border-blue-500',
+            isSelected: false,
+        },
+        {
+            name: 'drop',
+            textColor: 'text-orange-500',
+            borderColor: 'border-orange-500',
+            isSelected: false,
+        },
+        {
+            name: 'cluster',
+            textColor: 'text-amber-500',
+            borderColor: 'border-amber-500',
+            isSelected: false,
+        },
+        {
+            name: 'iso',
+            textColor: 'text-indigo-500',
+            borderColor: 'border-indigo-500',
+            isSelected: false,
+        },
+        {
+            name: 'partials',
+            textColor: 'text-fuchsia-500',
+            borderColor: 'border-fuchsia-500',
+            isSelected: false,
+        },
+        {
+            name: 'slow eccentric',
+            textColor: 'text-green-500',
+            borderColor: 'border-green-500',
+            isSelected: false,
+        },
+    ];
+
+    onSetTypeChange($index: number) {
+        this.setTypes.forEach((set) => (set.isSelected = false));
+        this.setTypes[$index].isSelected = true;
+        this.setTypeIndex = $index;
+
+        this.modalVisibility = false;
+    }
+}
