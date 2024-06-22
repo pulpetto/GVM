77e3be9a821e1d5b5711592af9421fa229996c55
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-22 20:34:43 +0200

    add dynamic index count

--- src/app/shared/exercise-edit-mode/set/drop-set/drop-set.component.ts
+++ src/app/shared/exercise-edit-mode/set/drop-set/drop-set.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ButtonForRpeModalComponent } from '../../../button-for-rpe-modal/button-for-rpe-modal.component';
@@ -11,6 +11,7 @@ import { ButtonForRpeModalComponent } from '../../../button-for-rpe-modal/button
     imports: [CommonModule, FormsModule, ButtonForRpeModalComponent],
 })
 export class DropSetComponent {
+    @Input({ required: true }) dropsetNumber!: number;
     @Output() rpeValueChangeEvent = new EventEmitter<number>();
 
     onRpeValueChange($event: number) {
