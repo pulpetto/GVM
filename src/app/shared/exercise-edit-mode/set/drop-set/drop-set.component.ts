6b433637e237d4a22d7713d8cc700316586dbf1d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-22 10:10:11 +0200

    add extracted code to the component

--- src/app/shared/exercise-edit-mode/set/drop-set/drop-set.component.ts
+++ src/app/shared/exercise-edit-mode/set/drop-set/drop-set.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-drop-set',
-  standalone: true,
-  imports: [],
-  templateUrl: './drop-set.component.html',
-  styleUrl: './drop-set.component.css'
+    selector: 'app-drop-set',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './drop-set.component.html',
+    styleUrl: './drop-set.component.css',
 })
 export class DropSetComponent {
+    weight!: number;
+    reps!: number;
+    rpe!: number;
 
+    rpeModalVisibility: boolean = false;
 }
