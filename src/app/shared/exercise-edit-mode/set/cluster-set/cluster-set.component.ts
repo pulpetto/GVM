5826b66278abca83f95987514a10cef843e0bd7c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-22 21:25:54 +0200

    add base code

--- src/app/shared/exercise-edit-mode/set/cluster-set/cluster-set.component.ts
+++ src/app/shared/exercise-edit-mode/set/cluster-set/cluster-set.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ButtonForRpeModalComponent } from '../../../button-for-rpe-modal/button-for-rpe-modal.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-cluster-set',
     standalone: true,
-    imports: [],
     templateUrl: './cluster-set.component.html',
     styleUrl: './cluster-set.component.css',
+    imports: [ButtonForRpeModalComponent, FormsModule],
 })
-export class ClusterSetComponent {}
+export class ClusterSetComponent {
+    @Output() inputValuesChangeEvent = new EventEmitter<{
+        restTime: number;
+        reps: number;
+        rpe: number | null;
+    }>();
+
+    updateRpeValue($event: number | null) {
+        this.rpe = $event;
+
+        this.onAnyValueChange();
+    }
+
+    onAnyValueChange() {
+        this.inputValuesChangeEvent.emit({
+            restTime: this.restTime,
+            reps: this.reps,
+            rpe: this.rpe,
+        });
+    }
+
+    restTime!: number;
+    reps!: number;
+    rpe!: number | null;
+
+    rpeModalVisibility: boolean = false;
+}
