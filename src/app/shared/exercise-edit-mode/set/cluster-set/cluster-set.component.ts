cdbb887d604e9d65e181597cace3dcf3747722b3
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-23 04:54:40 +0200

    add rest time modal

--- src/app/shared/exercise-edit-mode/set/cluster-set/cluster-set.component.ts
+++ src/app/shared/exercise-edit-mode/set/cluster-set/cluster-set.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    EventEmitter,
+    Output,
+    QueryList,
+    ViewChildren,
+} from '@angular/core';
 import { ButtonForRpeModalComponent } from '../../../button-for-rpe-modal/button-for-rpe-modal.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -12,7 +19,7 @@ import { CommonModule } from '@angular/common';
 })
 export class ClusterSetComponent {
     @Output() inputValuesChangeEvent = new EventEmitter<{
-        restTime: number;
+        restTime: number | null;
         reps: number;
         rpe: number | null;
     }>();
@@ -31,9 +38,45 @@ export class ClusterSetComponent {
         });
     }
 
-    restTime!: number;
+    restTime!: number | null;
     reps!: number;
     rpe!: number | null;
 
-    restTimeModalVisibility: boolean = false;
+    restTimeModalVisibility: boolean = true;
+    restTimesScale: number[] = [5, 10, 15, 20, 25, 30];
+
+    hoveredRestTimeValue: number | null = null;
+    hoverSquareLeftDistancePx: number = 0;
+    hoverSquareShadowLeftDistancePx: number = 0;
+
+    @ViewChildren('restTimeButton')
+    restTimesValuesButtons!: QueryList<ElementRef>;
+
+    onRestTimeChange(restTime: number, $index: number) {
+        if (this.restTime === restTime) {
+            this.restTime = null;
+            this.onAnyValueChange();
+            this.hoverSquareLeftDistancePx = 0;
+        } else {
+            this.restTime = restTime;
+            this.onAnyValueChange();
+            this.hoverSquareLeftDistancePx =
+                this.restTimesValuesButtons.toArray()[
+                    $index
+                ].nativeElement.offsetLeft;
+        }
+    }
+
+    onRestTimeMouseOver(restTime: number, $index: number) {
+        this.hoveredRestTimeValue = restTime;
+
+        this.hoverSquareShadowLeftDistancePx =
+            this.restTimesValuesButtons.toArray()[
+                $index
+            ].nativeElement.offsetLeft;
+    }
+
+    onRestTimeMouseOut() {
+        this.hoveredRestTimeValue = null;
+    }
 }
