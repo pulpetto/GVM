177bd22d4683307870a38862163da6de9e7dc6bc
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-23 10:54:10 +0200

    Create set-type.ts

---
+++ src/app/types/set-type.ts
@@ -0,0 +1,11 @@
+export type SetType =
+    | 'normal'
+    | 'warmup'
+    | 'failure'
+    | 'paused'
+    | 'drop'
+    | 'cluster'
+    | 'iso'
+    | 'partials'
+    | 'tempo'
+    | 'slow eccentric';
