f1234c0b2689c15c749515a3ef77642075c9342c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-25 07:46:34 +0200

    add modal layout & change property structure

--- src/app/shared/exercise-edit-mode/set/tempo-set/tempo-set.component.ts
+++ src/app/shared/exercise-edit-mode/set/tempo-set/tempo-set.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { NgxMaskDirective } from 'ngx-mask';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-tempo-set',
     standalone: true,
-    imports: [NgxMaskDirective, FormsModule],
+    imports: [NgxMaskDirective, FormsModule, CommonModule],
     templateUrl: './tempo-set.component.html',
     styleUrl: './tempo-set.component.css',
 })
 export class TempoSetComponent {
-    eccentricPhaseLength = 0;
-    isometricPhaseOneLength = 0;
-    concentricPhaseLength = 0;
-    isometricPhaseTwoLength = 0;
+    tempo!: {
+        eccentricPhaseLength: number;
+        isometricPhaseOneLength: number;
+        concentricPhaseLength: number;
+        isometricPhaseTwoLength: number;
+    };
 
-    tempoModalVisibility: boolean = false;
+    tempoModalVisibility: boolean = true;
 
     onTempoValueChange() {}
 }
