ac62289d8c2a8d9a3e1c6d35766e6a7a5ef709e6
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-25 07:19:34 +0200

    build initial structure

--- src/app/shared/exercise-edit-mode/set/tempo-set/tempo-set.component.ts
+++ src/app/shared/exercise-edit-mode/set/tempo-set/tempo-set.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { NgxMaskDirective } from 'ngx-mask';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-tempo-set',
     standalone: true,
-    imports: [],
+    imports: [NgxMaskDirective, FormsModule],
     templateUrl: './tempo-set.component.html',
     styleUrl: './tempo-set.component.css',
 })
-export class TempoSetComponent {}
+export class TempoSetComponent {
+    eccentricPhaseLength = 0;
+    isometricPhaseOneLength = 0;
+    concentricPhaseLength = 0;
+    isometricPhaseTwoLength = 0;
+
+    tempoModalVisibility: boolean = false;
+
+    onTempoValueChange() {}
+}
