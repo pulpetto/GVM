1a143206cbc3bc2077d6d57edcb118d6489b1b57
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-27 08:12:21 +0200

    disitll title edit logic into separate component

--- src/app/pages/user/workout/creator/creator.component.ts
+++ src/app/pages/user/workout/creator/creator.component.ts
@@ -1,49 +1,12 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { ExerciseEditModeComponent } from '../../../../shared/exercise-edit-mode/exercise-edit-mode.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
 
 @Component({
     selector: 'app-creator',
     standalone: true,
     templateUrl: './creator.component.html',
     styleUrl: './creator.component.css',
-    imports: [FormsModule, CommonModule, ExerciseEditModeComponent],
+    imports: [ExerciseEditModeComponent, NameEditorComponent],
 })
-export class CreatorComponent {
-    nameEditMode: boolean = false;
-    previousTitle: string = 'My Workout 1';
-    title: string = 'My Workout 1';
-
-    @ViewChild('inputRef') inputEl!: ElementRef;
-
-    @HostListener('document:keydown', ['$event'])
-    handleKeyboardEvent(event: KeyboardEvent) {
-        if (event.key === 'Escape') {
-            this.cancelNewTitle();
-        }
-
-        if (event.key === 'Enter') {
-            this.updateTitle();
-        }
-    }
-
-    openTitleEditMode() {
-        this.nameEditMode = true;
-        this.title = '';
-
-        setTimeout(() => {
-            this.inputEl.nativeElement.focus();
-        }, 0);
-    }
-
-    cancelNewTitle() {
-        this.nameEditMode = false;
-        this.title = this.previousTitle;
-    }
-
-    updateTitle() {
-        this.nameEditMode = false;
-        this.previousTitle = this.title;
-    }
-}
+export class CreatorComponent {}
--- src/app/pages/user/workout/creator/name-editor/name-editor.component.ts
+++ src/app/pages/user/workout/creator/name-editor/name-editor.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-name-editor',
     standalone: true,
-    imports: [],
+    imports: [FormsModule],
     templateUrl: './name-editor.component.html',
     styleUrl: './name-editor.component.css',
 })
-export class NameEditorComponent {}
+export class NameEditorComponent {
+    nameEditMode: boolean = false;
+    previousTitle: string = 'My Workout 1';
+    title: string = 'My Workout 1';
+
+    @ViewChild('inputRef') inputEl!: ElementRef;
+
+    @HostListener('document:keydown', ['$event'])
+    handleKeyboardEvent(event: KeyboardEvent) {
+        if (event.key === 'Escape') {
+            this.cancelNewTitle();
+        }
+
+        if (event.key === 'Enter') {
+            this.updateTitle();
+        }
+    }
+
+    openTitleEditMode() {
+        this.nameEditMode = true;
+        this.title = '';
+
+        setTimeout(() => {
+            this.inputEl.nativeElement.focus();
+        }, 0);
+    }
+
+    cancelNewTitle() {
+        this.nameEditMode = false;
+        this.title = this.previousTitle;
+    }
+
+    updateTitle() {
+        this.nameEditMode = false;
+        this.previousTitle = this.title;
+    }
+}
