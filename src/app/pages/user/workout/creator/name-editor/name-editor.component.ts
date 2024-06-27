3b0117e47b1b66fa49d9c76674fc250845fcaace
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-27 08:06:52 +0200

    add component

---
+++ src/app/pages/user/workout/creator/name-editor/name-editor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-name-editor',
+    standalone: true,
+    imports: [],
+    templateUrl: './name-editor.component.html',
+    styleUrl: './name-editor.component.css',
+})
+export class NameEditorComponent {}
