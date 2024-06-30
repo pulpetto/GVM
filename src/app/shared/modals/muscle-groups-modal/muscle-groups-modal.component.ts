f2dda218445481b36ac2ebc360e8a0fd38dffad0
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-06-30 20:12:23 +0200

    add import

--- src/app/shared/modals/muscle-groups-modal/muscle-groups-modal.component.ts
+++ src/app/shared/modals/muscle-groups-modal/muscle-groups-modal.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-muscle-groups-modal',
-  standalone: true,
-  imports: [],
-  templateUrl: './muscle-groups-modal.component.html',
-  styleUrl: './muscle-groups-modal.component.css'
+    selector: 'app-muscle-groups-modal',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './muscle-groups-modal.component.html',
+    styleUrl: './muscle-groups-modal.component.css',
 })
 export class MuscleGroupsModalComponent {
 
