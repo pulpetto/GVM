96a90e6a2318e4711970c9989601c1571454078a
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-07-01 20:25:42 +0200

    add component

---
+++ src/app/shared/modals/equipment-modal/equipment-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './equipment-modal.component.html',
+  styleUrl: './equipment-modal.component.css'
+})
+export class EquipmentModalComponent {
+
+}
