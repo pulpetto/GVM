73bc1a678dcd1e6ab9dce2c14f485f464f34b89a
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-07-01 20:49:42 +0200

    add content to the component

--- src/app/shared/modals/equipment-modal/equipment-modal.component.ts
+++ src/app/shared/modals/equipment-modal/equipment-modal.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
+import { DataService } from '../../../services/data.service';
+import { Equipment } from '../../../interfaces/equipment';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-equipment-modal',
-  standalone: true,
-  imports: [],
-  templateUrl: './equipment-modal.component.html',
-  styleUrl: './equipment-modal.component.css'
+    selector: 'app-equipment-modal',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './equipment-modal.component.html',
+    styleUrl: './equipment-modal.component.css',
 })
-export class EquipmentModalComponent {
+export class EquipmentModalComponent implements OnInit {
+    @Output() visibilityChangeEvent = new EventEmitter<boolean>();
 
+    visibility: boolean = false;
+    equipment!: Equipment[];
+    selectedEquipmentItemsIds = new Set<number>();
+
+    dataService = inject(DataService);
+
+    ngOnInit() {
+        this.dataService.getEquipment().subscribe((data) => {
+            this.equipment = data;
+        });
+    }
+
+    onOptionSelect($index: number) {
+        if (this.selectedEquipmentItemsIds.has($index + 1)) {
+            this.selectedEquipmentItemsIds.delete($index + 1);
+        } else {
+            this.selectedEquipmentItemsIds.add($index + 1);
+        }
+    }
+
+    openModal() {
+        this.visibilityChangeEvent.emit(true);
+        this.visibility = true;
+    }
+
+    closeModal() {
+        this.visibilityChangeEvent.emit(false);
+        this.visibility = false;
+    }
 }
