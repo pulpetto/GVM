c175fa5639aee4a1e0db701420f39b4f61495287
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-07-02 05:39:29 +0200

    add method

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,12 +3,17 @@ import { Injectable } from '@angular/core';
 import { MuscleGroup } from '../interfaces/muscle-group';
 import { Observable } from 'rxjs';
 import { Equipment } from '../interfaces/equipment';
+import { Exercise } from '../interfaces/exercise';
 
 @Injectable({
     providedIn: 'root',
 })
 export class DataService {
     constructor(private http: HttpClient) {}
+
+    getExercises(): Observable<Exercise[]> {
+        return this.http.get<Exercise[]>('assets/data/exercises.json');
+    }
 
     getMuscleGroups(): Observable<MuscleGroup[]> {
         return this.http.get<MuscleGroup[]>('assets/data/muscle-groups.json');
