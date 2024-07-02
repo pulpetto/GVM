a59dff6cb269186cf90de9f264cc0dad6c516f64
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-07-02 18:45:54 +0200

    add routing to exercise

--- src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
+++ src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
@@ -2,13 +2,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfoModalButtonComponent } from '../info-modal-button/info-modal-button.component';
 import { SetComponent } from './set/set.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-exercise-edit-mode',
     standalone: true,
     templateUrl: './exercise-edit-mode.component.html',
     styleUrl: './exercise-edit-mode.component.css',
-    imports: [CommonModule, InfoModalButtonComponent, SetComponent],
+    imports: [
+        CommonModule,
+        InfoModalButtonComponent,
+        SetComponent,
+        RouterModule,
+    ],
 })
 export class ExerciseEditModeComponent {
     @Output() exercisesRemoveEvent = new EventEmitter<string>();
