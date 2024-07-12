5d393d35145c88ef3295a64923398399216bb7da
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-07-12 18:52:05 +0200

    add animations

--- src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
+++ src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
@@ -11,6 +11,15 @@ import { InfoModalButtonComponent } from '../info-modal-button/info-modal-button
 import { SetComponent } from './set/set.component';
 import { RouterModule } from '@angular/router';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+const visibleModal = { top: '50%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-exercise-edit-mode',
@@ -22,6 +31,28 @@ import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
         InfoModalButtonComponent,
         SetComponent,
         RouterModule,
+    ],
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
     ],
 })
 export class ExerciseEditModeComponent implements OnInit {
