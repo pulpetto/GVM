65b1c89a8c4f7030059a26300b15ee1480ce991d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-07-12 18:46:10 +0200

    add animations

--- src/app/shared/modals/equipment-modal/equipment-modal.component.ts
+++ src/app/shared/modals/equipment-modal/equipment-modal.component.ts
@@ -2,6 +2,18 @@ import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { DataService } from '../../../services/data.service';
 import { Equipment } from '../../../interfaces/equipment';
 import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+const visibleModal = { top: '25%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const visibleBtnFixed = { bottom: '0' };
+const hiddenBtnFixed = { bottom: '-100%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-equipment-modal',
@@ -9,6 +21,38 @@ import { CommonModule } from '@angular/common';
     imports: [CommonModule],
     templateUrl: './equipment-modal.component.html',
     styleUrl: './equipment-modal.component.css',
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose3', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
+            ]),
+        ]),
+    ],
 })
 export class EquipmentModalComponent implements OnInit {
     @Output() visibilityChangeEvent = new EventEmitter<boolean>();
