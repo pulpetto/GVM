c4b43c2211b58d023d12b68d3777f7942ecc567b
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-07-13 05:20:32 +0200

    add animations

--- src/app/shared/info-modal-button/info-modal-button.component.ts
+++ src/app/shared/info-modal-button/info-modal-button.component.ts
@@ -1,5 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+const visibleModal = { top: '75%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-info-modal-button',
@@ -7,6 +16,28 @@ import { CommonModule } from '@angular/common';
     imports: [CommonModule],
     templateUrl: './info-modal-button.component.html',
     styleUrl: './info-modal-button.component.css',
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+    ],
 })
 export class InfoModalButtonComponent {
     @Input({ required: true }) name!: string;
