71753c9c0b5d6388ebfa632f0c48dc4dc852b0c5
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-07-13 19:50:12 +0200

    add options modal window

--- src/app/shared/workout-split/workout-split.component.ts
+++ src/app/shared/workout-split/workout-split.component.ts
@@ -1,6 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { WorkoutMiniPreviewComponent } from '../workoutViews/workout-mini-preview/workout-mini-preview.component';
 import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+const visibleModal = { top: '75%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-workout-split',
@@ -8,11 +17,34 @@ import { CommonModule } from '@angular/common';
     templateUrl: './workout-split.component.html',
     styleUrl: './workout-split.component.css',
     imports: [WorkoutMiniPreviewComponent, CommonModule],
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+    ],
 })
 export class WorkoutSplitComponent {
     @Input({ required: true }) splitName!: string;
     @Input({ required: true }) workoutsNames!: string[];
     isOpen: boolean = false;
+    optionsModalVisibility: boolean = true;
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     calculateHeight(element: any) {
@@ -33,5 +65,9 @@ export class WorkoutSplitComponent {
         } else {
             element.style.height = '0px';
         }
+    }
+
+    openOptionsModal() {
+        this.optionsModalVisibility = true;
     }
 }
