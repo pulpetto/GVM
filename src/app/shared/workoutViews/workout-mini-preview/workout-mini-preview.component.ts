ee64935b66c3fce9716f6bda78fb881cff3d1913
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-07-19 10:12:21 +0200

    get workout name

--- src/app/shared/workoutViews/workout-mini-preview/workout-mini-preview.component.ts
+++ src/app/shared/workoutViews/workout-mini-preview/workout-mini-preview.component.ts
@@ -1,16 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonForModalComponent } from '../../button-for-modal/button-for-modal.component';
+import { UserService } from '../../../services/user.service';
+import { map, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-workout-mini-preview',
     standalone: true,
     templateUrl: './workout-mini-preview.component.html',
     styleUrl: './workout-mini-preview.component.css',
-    imports: [RouterModule, ButtonForModalComponent],
+    imports: [RouterModule, ButtonForModalComponent, CommonModule],
 })
-export class WorkoutMiniPreviewComponent {
+export class WorkoutMiniPreviewComponent implements OnInit {
     @Input({ required: true }) workoutId!: string;
+
+    userService = inject(UserService);
+
+    workoutName$!: Observable<string>;
+
+    ngOnInit() {
+        this.workoutName$ = this.userService
+            .getWorkoutById(this.workoutId)
+            .pipe(map((workout) => workout.name));
+    }
 
     onMenuClick($event: Event) {
         $event.stopPropagation();
