c655876b7009ed689c3729f7282f94d27a47f18d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-01 09:31:57 +0200

    get workout data

--- src/app/shared/workoutViews/workout-full-view/workout-full-view.component.ts
+++ src/app/shared/workoutViews/workout-full-view/workout-full-view.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { ChartsCarouselComponent } from '../../charts-carousel/charts-carousel.component';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../../services/user.service';
+import { Observable } from 'rxjs';
+import { Workout } from '../../../interfaces/workout/workout';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-workout-full-view',
@@ -9,4 +14,32 @@ import { CommonModule } from '@angular/common';
     styleUrl: './workout-full-view.component.css',
     imports: [ChartsCarouselComponent, CommonModule],
 })
-export class WorkoutFullViewComponent {}
+export class WorkoutFullViewComponent implements OnInit {
+    workoutId!: string;
+    workoutData$!: Observable<Workout>;
+
+    destroyRef = inject(DestroyRef);
+
+    constructor(
+        private route: ActivatedRoute,
+        private userService: UserService
+    ) {}
+
+    ngOnInit() {
+        this.route.paramMap
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((params) => {
+                this.workoutId = params.get('id')!;
+            });
+
+        this.userService.user$
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((user) => {
+                if (user && this.workoutId) {
+                    this.workoutData$ = this.userService.getWorkoutById(
+                        this.workoutId
+                    );
+                }
+            });
+    }
+}
