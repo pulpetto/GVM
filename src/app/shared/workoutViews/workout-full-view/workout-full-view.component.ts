ae32d3c33fe7cb0c5be7c64790f1036bc461e0bf
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-01 19:38:02 +0200

    add empty modal with animations

--- src/app/shared/workoutViews/workout-full-view/workout-full-view.component.ts
+++ src/app/shared/workoutViews/workout-full-view/workout-full-view.component.ts
@@ -6,6 +6,18 @@ import { UserService } from '../../../services/user.service';
 import { Observable } from 'rxjs';
 import { Workout } from '../../../interfaces/workout/workout';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const visibleModal = { top: '25%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const visibleBtnFixed = { bottom: '0' };
+const hiddenBtnFixed = { bottom: '-100%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-workout-full-view',
@@ -13,9 +25,42 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
     templateUrl: './workout-full-view.component.html',
     styleUrl: './workout-full-view.component.css',
     imports: [ChartsCarouselComponent, CommonModule],
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose3', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
+            ]),
+        ]),
+    ],
 })
 export class WorkoutFullViewComponent implements OnInit {
     workoutId!: string;
+    optionsModalVisibility: boolean = false;
     workoutData$!: Observable<Workout>;
 
     destroyRef = inject(DestroyRef);
