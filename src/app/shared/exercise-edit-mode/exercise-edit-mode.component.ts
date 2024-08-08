bace00e6bba3195607e4394901b5090e190e4ab7
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-08 08:29:52 +0200

    change design for options modal

--- src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
+++ src/app/shared/exercise-edit-mode/exercise-edit-mode.component.ts
@@ -12,6 +12,9 @@ const hiddenModal = { top: '100%' };
 const visibleBg = { opacity: '100%' };
 const hiddenBg = { opacity: '0%' };
 
+const visibleBtnFixed = { bottom: '0' };
+const hiddenBtnFixed = { bottom: '-100%' };
+
 const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
@@ -44,6 +47,16 @@ const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
             transition(':leave', [
                 style(visibleBg),
                 animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose3', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
             ]),
         ]),
     ],
