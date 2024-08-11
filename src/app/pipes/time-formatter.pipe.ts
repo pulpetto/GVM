b6732931402acb16b4ead1c596c1a3554d362158
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-11 18:33:33 +0200

    add pipe

---
+++ src/app/pipes/time-formatter.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'timeFormatter',
+    standalone: true,
+})
+export class TimeFormatterPipe implements PipeTransform {
+    transform(value: number): string {
+        const hours = Math.floor(value / 3600);
+        const minutes = Math.floor((value % 3600) / 60);
+        const seconds = value % 60;
+
+        let formattedTime = '';
+
+        if (hours > 0) {
+            formattedTime += `${hours}h `;
+        }
+
+        if (minutes > 0 || hours > 0) {
+            formattedTime += `${minutes}m `;
+        }
+
+        formattedTime += `${seconds}s`;
+
+        return formattedTime;
+    }
+}
