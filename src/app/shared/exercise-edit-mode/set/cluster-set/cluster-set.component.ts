2d7b3c9d37ed049c57564a8551e4f94aa1309dc4
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-13 09:56:41 +0200

    optimize modal performance

--- src/app/shared/exercise-edit-mode/set/cluster-set/cluster-set.component.ts
+++ src/app/shared/exercise-edit-mode/set/cluster-set/cluster-set.component.ts
@@ -12,6 +12,18 @@ import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 import { CommonModule, DOCUMENT } from '@angular/common';
 import { RpeType } from '../../../../types/rpe-type';
 import { ReactiveFormsModule } from '@angular/forms';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const visibleModal = { top: '50%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const visibleBtnFixed = { bottom: '0' };
+const hiddenBtnFixed = { bottom: '-100%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-cluster-set',
@@ -23,6 +35,38 @@ import { ReactiveFormsModule } from '@angular/forms';
         FormsModule,
         CommonModule,
         ReactiveFormsModule,
+    ],
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose3', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
+            ]),
+        ]),
     ],
 })
 export class ClusterSetComponent {
