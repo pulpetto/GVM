f2e492515fe0c5302683b5ec3c0c675881a6b962
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-17 19:22:01 +0200

    get exercises data

--- src/app/shared/workoutViews/workout-done-preview/workout-done-preview.component.ts
+++ src/app/shared/workoutViews/workout-done-preview/workout-done-preview.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnInit } from '@angular/core';
 import { WorkoutDone } from '../../../interfaces/workout/workout-done';
 import { TimeFormatterPipe } from '../../../pipes/time-formatter.pipe';
+import { DataService } from '../../../services/data.service';
+import { Exercise } from '../../../interfaces/exercise';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-workout-done-preview',
@@ -9,6 +12,20 @@ import { TimeFormatterPipe } from '../../../pipes/time-formatter.pipe';
     templateUrl: './workout-done-preview.component.html',
     styleUrl: './workout-done-preview.component.css',
 })
-export class WorkoutDonePreviewComponent {
+export class WorkoutDonePreviewComponent implements OnInit {
     @Input({ required: true }) workoutData!: WorkoutDone;
+
+    dataService = inject(DataService);
+    destroyRef = inject(DestroyRef);
+
+    exercisesData: Exercise[] = [];
+
+    ngOnInit() {
+        this.workoutData.exercises.forEach((exercise) => {
+            this.dataService
+                .getExerciseById(exercise.exerciseId)
+                .pipe(takeUntilDestroyed(this.destroyRef))
+                .subscribe((data) => this.exercisesData.push(data!));
+        });
+    }
 }
