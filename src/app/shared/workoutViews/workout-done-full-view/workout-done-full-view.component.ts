6b881c9498ac4daf2a95be3e199ab9019ddad76f
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-19 11:21:50 +0200

    add animations data

--- src/app/shared/workoutViews/workout-done-full-view/workout-done-full-view.component.ts
+++ src/app/shared/workoutViews/workout-done-full-view/workout-done-full-view.component.ts
@@ -3,6 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ChartsCarouselComponent } from '../../charts-carousel/charts-carousel.component';
 import { RouterModule } from '@angular/router';
 import { WorkoutDoneWithId } from '../../../interfaces/workout/workout-done-with-id';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const visibleModal = { top: '50%' };
+const visibleModalLow = { top: '75%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const visibleBtnFixed = { bottom: '0' };
+const hiddenBtnFixed = { bottom: '-100%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-workout-done-full-view',
@@ -10,6 +23,68 @@ import { WorkoutDoneWithId } from '../../../interfaces/workout/workout-done-with
     imports: [CommonModule, ChartsCarouselComponent, RouterModule],
     templateUrl: './workout-done-full-view.component.html',
     styleUrl: './workout-done-full-view.component.css',
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose3', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
+            ]),
+        ]),
+        trigger('openClose4', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModalLow)),
+            ]),
+            transition(':leave', [
+                style(visibleModalLow),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose5', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose6', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
+            ]),
+        ]),
+    ],
 })
 export class WorkoutDoneFullViewComponent implements OnInit {
     optionsModalVisibility: boolean = false;
