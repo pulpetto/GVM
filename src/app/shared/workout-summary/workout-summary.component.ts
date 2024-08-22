b6c463fd8907050eff093aecebb0ae5ccd1617a5
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-22 11:17:23 +0200

    add initial view styling

--- src/app/shared/workout-summary/workout-summary.component.ts
+++ src/app/shared/workout-summary/workout-summary.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabsComponent } from '../tabs/tabs.component';
 
 @Component({
     selector: 'app-workout-summary',
     standalone: true,
-    imports: [],
+    imports: [CommonModule, TabsComponent],
     templateUrl: './workout-summary.component.html',
     styleUrl: './workout-summary.component.css',
 })
