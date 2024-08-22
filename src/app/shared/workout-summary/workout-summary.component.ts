ce3f46b7c4977fca5549822870cdd572ef0ced15
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-22 05:17:46 +0200

    add component

---
+++ src/app/shared/workout-summary/workout-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-workout-summary',
+    standalone: true,
+    imports: [],
+    templateUrl: './workout-summary.component.html',
+    styleUrl: './workout-summary.component.css',
+})
+export class WorkoutSummaryComponent {}
