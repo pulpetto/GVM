c5d3948a05cefd5b37c91e93af6e95dfb4b87cd1
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-25 10:00:56 +0200

    replace content with component

--- src/app/shared/workout-summary/workout-summary.component.ts
+++ src/app/shared/workout-summary/workout-summary.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from '../tabs/tabs.component';
+import { MotivationalQuotesComponent } from './motivational-quotes/motivational-quotes.component';
 
 @Component({
     selector: 'app-workout-summary',
     standalone: true,
-    imports: [CommonModule, TabsComponent],
+    imports: [CommonModule, TabsComponent, MotivationalQuotesComponent],
     templateUrl: './workout-summary.component.html',
     styleUrl: './workout-summary.component.css',
 })
