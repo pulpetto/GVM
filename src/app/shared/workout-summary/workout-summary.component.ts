a0e661a98ce898d9f2e5884df9974dd1eb17fe23
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-25 14:31:51 +0200

    add component

--- src/app/shared/workout-summary/workout-summary.component.ts
+++ src/app/shared/workout-summary/workout-summary.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from '../tabs/tabs.component';
 import { MotivationalQuotesComponent } from './motivational-quotes/motivational-quotes.component';
+import { FeedbackComponent } from './tabs/feedback/feedback.component';
 
 @Component({
     selector: 'app-workout-summary',
     standalone: true,
-    imports: [CommonModule, TabsComponent, MotivationalQuotesComponent],
+    imports: [
+        CommonModule,
+        TabsComponent,
+        MotivationalQuotesComponent,
+        FeedbackComponent,
+    ],
     templateUrl: './workout-summary.component.html',
     styleUrl: './workout-summary.component.css',
 })
