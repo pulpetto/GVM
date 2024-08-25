f1a95cf429a35d355024ffdbfede284671f1dd50
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-25 09:58:59 +0200

    add component

---
+++ src/app/shared/workout-summary/motivational-quotes/motivational-quotes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-motivational-quotes',
+  standalone: true,
+  imports: [],
+  templateUrl: './motivational-quotes.component.html',
+  styleUrl: './motivational-quotes.component.css'
+})
+export class MotivationalQuotesComponent {
+
+}
