273821129a003ac893853110c34a38f6f92c165a
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-26 15:13:07 +0200

    add component

---
+++ src/app/shared/workout-summary/tabs/progress/progress.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-progress',
+    standalone: true,
+    imports: [],
+    templateUrl: './progress.component.html',
+    styleUrl: './progress.component.css',
+})
+export class ProgressComponent {}
