99be1b9ed62dbd4b75648d539b8ac00c1b10c877
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-26 15:13:47 +0200

    disitll markup into separate component

--- src/app/shared/workout-summary/workout-summary.component.ts
+++ src/app/shared/workout-summary/workout-summary.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TabsComponent } from '../tabs/tabs.component';
 import { MotivationalQuotesComponent } from './motivational-quotes/motivational-quotes.component';
 import { FeedbackComponent } from './tabs/feedback/feedback.component';
+import { ProgressComponent } from './tabs/progress/progress.component';
 
 @Component({
     selector: 'app-workout-summary',
@@ -12,6 +13,7 @@ import { FeedbackComponent } from './tabs/feedback/feedback.component';
         TabsComponent,
         MotivationalQuotesComponent,
         FeedbackComponent,
+        ProgressComponent,
     ],
     templateUrl: './workout-summary.component.html',
     styleUrl: './workout-summary.component.css',
