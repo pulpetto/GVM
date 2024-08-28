6a7db9bb257cb7e4d8b638469072e1fc044c4634
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-28 18:06:24 +0200

    add component

---
+++ src/app/pages/user/profile/goals/goal/goal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-goal',
+    standalone: true,
+    imports: [],
+    templateUrl: './goal.component.html',
+    styleUrl: './goal.component.css',
+})
+export class GoalComponent {}
