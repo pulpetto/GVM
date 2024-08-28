2ec30e98999c9ec23dd8c36f57fbd233cb4e1abc
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-08-28 08:46:42 +0200

    add tab switching

--- src/app/pages/user/profile/goals/goals.component.ts
+++ src/app/pages/user/profile/goals/goals.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    ElementRef,
+    inject,
+    QueryList,
+    ViewChildren,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-goals',
-  standalone: true,
-  imports: [],
-  templateUrl: './goals.component.html',
-  styleUrl: './goals.component.css'
+    selector: 'app-goals',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './goals.component.html',
+    styleUrl: './goals.component.css',
 })
-export class GoalsComponent {
+export class GoalsComponent implements AfterViewInit {
+    cdr = inject(ChangeDetectorRef);
 
+    @ViewChildren('tabButton')
+    tabButtons!: QueryList<ElementRef>;
+
+    activeTabName: 'current' | 'done' = 'current';
+    tabWidthPx!: number;
+
+    ngAfterViewInit() {
+        this.tabWidthPx =
+            this.tabButtons.toArray()[0].nativeElement.clientWidth;
+
+        this.cdr.detectChanges();
+    }
+
+    switchTab(tabName: string) {
+        this.activeTabName = tabName as 'current' | 'done';
+    }
 }
