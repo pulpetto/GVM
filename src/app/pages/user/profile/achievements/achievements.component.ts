73a6b4171d578c2dae285eb3a4bb608b30678b12
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-02 07:15:03 +0200

    get data & pass it down

--- src/app/pages/user/profile/achievements/achievements.component.ts
+++ src/app/pages/user/profile/achievements/achievements.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AchievementComponent } from '../../../../shared/achievement/achievement.component';
+import { DataService } from '../../../../services/data.service';
+import { Observable } from 'rxjs';
+import { Achievement } from '../../../../interfaces/achievement';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-achievements',
     standalone: true,
-    imports: [AchievementComponent],
+    imports: [AchievementComponent, CommonModule],
     templateUrl: './achievements.component.html',
     styleUrl: './achievements.component.css',
 })
-export class AchievementsComponent {}
+export class AchievementsComponent implements OnInit {
+    dataService = inject(DataService);
+
+    achievements$!: Observable<Achievement[]>;
+
+    ngOnInit() {
+        this.achievements$ = this.dataService.getAchievements();
+    }
+}
