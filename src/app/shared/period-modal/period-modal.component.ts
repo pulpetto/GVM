3eab7a0fbb7cd89e059ab470408b18d35a406e1c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-06 04:47:45 +0200

    add markup with animations

--- src/app/shared/period-modal/period-modal.component.ts
+++ src/app/shared/period-modal/period-modal.component.ts
@@ -1,10 +1,68 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const visibleModal = { top: '50%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const visibleBtnFixed = { bottom: '0' };
+const hiddenBtnFixed = { bottom: '-100%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-period-modal',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './period-modal.component.html',
     styleUrl: './period-modal.component.css',
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose3', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
+            ]),
+        ]),
+    ],
 })
-export class PeriodModalComponent {}
+export class PeriodModalComponent {
+    modalVisibility: boolean = true;
+
+    activePeriod!: string;
+
+    periods = [
+        'Last Week',
+        'Last Month',
+        'Last 3 Months',
+        'Last 6 Months',
+        'Last Year',
+        'All time',
+    ];
+}
