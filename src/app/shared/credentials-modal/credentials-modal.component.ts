f6cfcbef981906f44460e46cef67f71e26c3f96d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-07 14:11:31 +0200

    add markup with animations

--- src/app/shared/credentials-modal/credentials-modal.component.ts
+++ src/app/shared/credentials-modal/credentials-modal.component.ts
@@ -1,10 +1,57 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const visibleModal = { top: '50%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const visibleBtnFixed = { bottom: '0' };
+const hiddenBtnFixed = { bottom: '-100%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-credentials-modal',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './credentials-modal.component.html',
     styleUrl: './credentials-modal.component.css',
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose3', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
+            ]),
+        ]),
+    ],
 })
-export class CredentialsModalComponent {}
+export class CredentialsModalComponent {
+    modalVisibility: boolean = true;
+}
