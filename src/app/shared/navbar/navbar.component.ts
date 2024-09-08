fb937cc8700b7d52c396a5a0454d7ee1690306e5
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-08 08:39:51 +0200

    get user role & display admin panel icon

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    imports: [RouterLink, RouterModule],
+    imports: [RouterLink, RouterModule, CommonModule],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    userService = inject(UserService);
+
+    userRole = computed(() => this.userService.currentUser()?.role);
+}
