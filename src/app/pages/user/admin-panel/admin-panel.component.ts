c7c1bf39829ddffe3d4d7c7eaab28cf9a23baaf4
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-08 13:47:49 +0200

    add import & routes

--- src/app/pages/user/admin-panel/admin-panel.component.ts
+++ src/app/pages/user/admin-panel/admin-panel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-admin-panel',
     standalone: true,
-    imports: [],
+    imports: [RouterModule],
     templateUrl: './admin-panel.component.html',
     styleUrl: './admin-panel.component.css',
 })
