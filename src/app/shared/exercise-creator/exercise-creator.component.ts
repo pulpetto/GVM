36f2251f71a77eb2d9db13706aa9af86fe197916
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-09 18:14:53 +0200

    adding thumbnail logic

--- src/app/shared/exercise-creator/exercise-creator.component.ts
+++ src/app/shared/exercise-creator/exercise-creator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-exercise-creator',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
     templateUrl: './exercise-creator.component.html',
     styleUrl: './exercise-creator.component.css',
 })
-export class ExerciseCreatorComponent {}
+export class ExerciseCreatorComponent {
+    @ViewChild('thumbnail') thumbnailInput!: ElementRef<HTMLInputElement>;
+
+    selectedThumbnail: string | ArrayBuffer | null = null;
+
+    onThumbnailSelect() {
+        if (this.thumbnailInput.nativeElement.files) {
+            const thumbnail = this.thumbnailInput.nativeElement.files[0];
+            const reader = new FileReader();
+
+            reader.onload = () => {
+                this.selectedThumbnail = reader.result;
+            };
+
+            reader.readAsDataURL(thumbnail);
+        }
+    }
+}
