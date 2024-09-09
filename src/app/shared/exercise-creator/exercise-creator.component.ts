3083b2f197b61d2ddecedd65387f838bc5581d2f
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-09 06:48:19 +0200

    add exercise thumbnail input

--- src/app/shared/exercise-creator/exercise-creator.component.ts
+++ src/app/shared/exercise-creator/exercise-creator.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-exercise-creator',
-  standalone: true,
-  imports: [],
-  templateUrl: './exercise-creator.component.html',
-  styleUrl: './exercise-creator.component.css'
+    selector: 'app-exercise-creator',
+    standalone: true,
+    imports: [],
+    templateUrl: './exercise-creator.component.html',
+    styleUrl: './exercise-creator.component.css',
 })
-export class ExerciseCreatorComponent {
-
-}
+export class ExerciseCreatorComponent {}
