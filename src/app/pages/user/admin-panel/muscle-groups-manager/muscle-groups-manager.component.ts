d37d5e3a130c790adea41088601209e561d9d55d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-10 15:11:33 +0200

    add modal

--- src/app/pages/user/admin-panel/muscle-groups-manager/muscle-groups-manager.component.ts
+++ src/app/pages/user/admin-panel/muscle-groups-manager/muscle-groups-manager.component.ts
@@ -1,10 +1,57 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+const visibleModal = { top: '50%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const visibleBtnFixed = { bottom: '0' };
+const hiddenBtnFixed = { bottom: '-100%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-muscle-groups-manager',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './muscle-groups-manager.component.html',
     styleUrl: './muscle-groups-manager.component.css',
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose3', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
+            ]),
+        ]),
+    ],
 })
-export class MuscleGroupsManagerComponent {}
+export class MuscleGroupsManagerComponent {
+    newMuscleGroupModalVisibility: boolean = false;
+}
