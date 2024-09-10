898a908f229bfb8e143801dae1e3da69ffb42472
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-10 13:53:45 +0200

    add video input

--- src/app/shared/exercise-creator/exercise-creator.component.ts
+++ src/app/shared/exercise-creator/exercise-creator.component.ts
@@ -9,8 +9,10 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class ExerciseCreatorComponent {
     @ViewChild('thumbnail') thumbnailInput!: ElementRef<HTMLInputElement>;
+    @ViewChild('video') videoInput!: ElementRef<HTMLInputElement>;
 
     selectedThumbnail: string | ArrayBuffer | null = null;
+    selectedVideo: string | ArrayBuffer | null = null;
 
     onThumbnailSelect() {
         if (this.thumbnailInput.nativeElement.files) {
@@ -27,5 +29,22 @@ export class ExerciseCreatorComponent {
 
     removeThumbnail() {
         this.selectedThumbnail = null;
+    }
+
+    onVideoSelect() {
+        if (this.videoInput.nativeElement.files) {
+            const video = this.videoInput.nativeElement.files[0];
+            const reader = new FileReader();
+
+            reader.onload = () => {
+                this.selectedVideo = reader.result;
+            };
+
+            reader.readAsDataURL(video);
+        }
+    }
+
+    removeVideo() {
+        this.selectedVideo = null;
     }
 }
