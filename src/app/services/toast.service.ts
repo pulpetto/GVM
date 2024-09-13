e3638de7ad7a39fc666cfcccdf7100d6b902a307
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-13 17:54:10 +0200

    add toast logic

--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Signal, signal } from '@angular/core';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ToastService {
-    constructor() {}
+    toast = signal<{ message: string; error: boolean } | null>(null);
+
+    show(message: string, error: boolean) {
+        this.toast.set({ message, error });
+
+        setTimeout(() => {
+            this.hide();
+        }, 3000);
+    }
+
+    hide() {
+        this.toast.set(null);
+    }
 }
