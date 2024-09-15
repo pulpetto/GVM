1b9a7254ab9a8c8ec513d7f67091a6a6e70dedde
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-15 04:47:09 +0200

    add animation

--- src/app/shared/toast-window/toast-window.component.ts
+++ src/app/shared/toast-window/toast-window.component.ts
@@ -1,6 +1,12 @@
 import { Component, computed, inject, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastService } from '../../services/toast.service';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const visible = { right: '50%', opacity: '1' };
+const hidden = { right: '-100%', opacity: '0' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-toast-window',
@@ -8,6 +14,18 @@ import { ToastService } from '../../services/toast.service';
     imports: [CommonModule],
     templateUrl: './toast-window.component.html',
     styleUrl: './toast-window.component.css',
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hidden),
+                animate(timing, style(visible)),
+            ]),
+            transition(':leave', [
+                style(visible),
+                animate(timing, style(hidden)),
+            ]),
+        ]),
+    ],
 })
 export class ToastWindowComponent {
     toastService = inject(ToastService);
