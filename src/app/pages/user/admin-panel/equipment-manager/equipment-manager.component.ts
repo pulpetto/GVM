d1318c74eed5e460a42e7892d67f935df276a449
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-20 13:45:57 +0200

    add component

---
+++ src/app/pages/user/admin-panel/equipment-manager/equipment-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-equipment-manager',
+    standalone: true,
+    imports: [],
+    templateUrl: './equipment-manager.component.html',
+    styleUrl: './equipment-manager.component.css',
+})
+export class EquipmentManagerComponent {}
