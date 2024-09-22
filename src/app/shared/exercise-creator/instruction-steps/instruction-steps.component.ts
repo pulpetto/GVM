cf76320c287ad6c4c9ffcdffc16d74bd339fc9de
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-22 07:44:50 +0200

    add initial structure with presentational data

--- src/app/shared/exercise-creator/instruction-steps/instruction-steps.component.ts
+++ src/app/shared/exercise-creator/instruction-steps/instruction-steps.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-instruction-steps',
-  standalone: true,
-  imports: [],
-  templateUrl: './instruction-steps.component.html',
-  styleUrl: './instruction-steps.component.css'
+    selector: 'app-instruction-steps',
+    standalone: true,
+    imports: [],
+    templateUrl: './instruction-steps.component.html',
+    styleUrl: './instruction-steps.component.css',
 })
 export class InstructionStepsComponent {
-
+    steps: {
+        stepName: string;
+        subSteps: string[];
+    }[] = [
+        {
+            stepName: 'Setup',
+            subSteps: [
+                'Stand with your feet hip-width apart and the bar over your mid-foot.',
+                'Bend down and grip the bar just outside your knees.',
+                'Keep your back straight and chest up.',
+            ],
+        },
+        {
+            stepName: 'Lift',
+            subSteps: [
+                'Push through your heels and stand up, lifting the bar.',
+                'Keep the bar close to your body.',
+                'Stand straight with the bar at thigh level.',
+            ],
+        },
+        {
+            stepName: 'Lockout',
+            subSteps: [
+                'Stand tall with your hips and knees fully extended.',
+                'Keep your shoulders back.',
+            ],
+        },
+        {
+            stepName: 'Descent',
+            subSteps: [
+                'Push your hips back and bend your knees to lower the bar.',
+                'Keep the bar close to your body.',
+                'Lower the bar to the ground.',
+            ],
+        },
+        {
+            stepName: 'Reset',
+            subSteps: [
+                'Ensure your setup is correct before lifting again.',
+                'Repeat the steps for additional repetitions.',
+            ],
+        },
+    ];
 }
