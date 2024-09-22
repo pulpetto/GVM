100318eff23c0aacf593f4025cc8a17002336112
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-22 07:50:04 +0200

    add component

---
+++ src/app/shared/exercise-creator/instruction-steps/step/step.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-step',
+    standalone: true,
+    imports: [],
+    templateUrl: './step.component.html',
+    styleUrl: './step.component.css',
+})
+export class StepComponent {}
