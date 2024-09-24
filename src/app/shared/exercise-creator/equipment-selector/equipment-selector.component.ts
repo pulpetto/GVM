5a1dc383be2ab5e94c993da80dd2144a2af7a880
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-24 10:47:00 +0200

    add component

---
+++ src/app/shared/exercise-creator/equipment-selector/equipment-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-equipment-selector',
+    standalone: true,
+    imports: [],
+    templateUrl: './equipment-selector.component.html',
+    styleUrl: './equipment-selector.component.css',
+})
+export class EquipmentSelectorComponent {}
