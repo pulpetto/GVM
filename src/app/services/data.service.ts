4ae1c7d1a33d8eff212e0236874e5ee4d73f2b6d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-25 06:47:17 +0200

    add method

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,15 +1,25 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { MuscleGroup } from '../interfaces/muscle-group';
 import { map, Observable } from 'rxjs';
 import { Equipment } from '../interfaces/equipment';
 import { Exercise } from '../interfaces/exercise';
 import { Achievement } from '../interfaces/achievement';
+import { EquipmentName } from '../types/equipment-type';
+import {
+    collection,
+    CollectionReference,
+    Firestore,
+    onSnapshot,
+    query,
+} from '@angular/fire/firestore';
 
 @Injectable({
     providedIn: 'root',
 })
 export class DataService {
+    firestore = inject(Firestore);
+
     constructor(private http: HttpClient) {}
 
     getExercises(): Observable<Exercise[]> {
@@ -32,8 +42,61 @@ export class DataService {
         return this.http.get<MuscleGroup[]>('assets/data/muscle-groups.json');
     }
 
-    getEquipment(): Observable<Equipment[]> {
-        return this.http.get<Equipment[]>('assets/data/equipment.json');
+    getEquipment(): Observable<
+        {
+            id: number;
+            name: EquipmentName;
+            imageUrl: string;
+        }[]
+    > {
+        return this.http.get<
+            {
+                id: number;
+                name: EquipmentName;
+                imageUrl: string;
+            }[]
+        >('assets/data/equipment.json');
+    }
+
+    getEquipment2(): Observable<Equipment[]> {
+        const equipmentRef: CollectionReference = collection(
+            this.firestore,
+            'equipment'
+        );
+
+        const orderedEquipmentQuery = query(equipmentRef);
+
+        // prettier-ignore
+        return new Observable<{
+            id: string;
+            name: string;
+            imageUrl: string;
+        }[]>((observer) => {
+            const unsubscribe = onSnapshot(
+                orderedEquipmentQuery,
+                (querySnapshot) => {
+                    const equipment = querySnapshot.docs.map(
+                        (doc) =>
+                            ({
+                                id: doc.id,
+                                name: doc.data()['name'],
+                                imageUrl: doc.data()['imagePreviewUrl'],
+                            } as {
+                                id: string;
+                                name: string;
+                                imageUrl: string;
+                            })
+                    );
+
+                    observer.next(equipment);
+                },
+                (error) => {
+                    observer.error(error);
+                }
+            );
+
+            return { unsubscribe };
+        });
     }
 
     getAchievements(): Observable<Achievement[]> {
