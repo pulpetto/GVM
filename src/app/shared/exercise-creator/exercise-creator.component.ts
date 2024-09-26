67c080593fc563c08d8a08c85ae4c8d639d2a91c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-26 08:18:07 +0200

    add form and connect fields

--- src/app/shared/exercise-creator/exercise-creator.component.ts
+++ src/app/shared/exercise-creator/exercise-creator.component.ts
@@ -1,8 +1,14 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { InstructionStepsComponent } from './instruction-steps/instruction-steps.component';
 import { EquipmentSelectorComponent } from './equipment-selector/equipment-selector.component';
 import { MuscleGroupSelectorComponent } from './muscle-group-selector/muscle-group-selector.component';
 import { RouterModule } from '@angular/router';
+import {
+    FormBuilder,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 
 @Component({
     selector: 'app-exercise-creator',
@@ -12,21 +18,38 @@ import { RouterModule } from '@angular/router';
         EquipmentSelectorComponent,
         MuscleGroupSelectorComponent,
         RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
     ],
     templateUrl: './exercise-creator.component.html',
     styleUrl: './exercise-creator.component.css',
 })
 export class ExerciseCreatorComponent {
+    fb = inject(FormBuilder);
+
     @ViewChild('thumbnail') thumbnailInput!: ElementRef<HTMLInputElement>;
     @ViewChild('video') videoInput!: ElementRef<HTMLInputElement>;
 
     selectedThumbnail: string | ArrayBuffer | null = null;
     selectedVideo: string | ArrayBuffer | null = null;
 
+    exerciseForm = this.fb.group({
+        name: ['', Validators.required],
+        thumbnailFile: this.fb.control<File | null>(null),
+        mainMuscleGroupsIds: this.fb.array<string>([]),
+        secondaryMuscleGroupsIds: this.fb.array<string>([]),
+        equipmentId: this.fb.control<string | null>(null),
+        videoFile: this.fb.control<File | null>(null),
+    });
+
     onThumbnailSelect() {
         if (this.thumbnailInput.nativeElement.files) {
             const thumbnail = this.thumbnailInput.nativeElement.files[0];
             const reader = new FileReader();
+
+            this.exerciseForm.patchValue({
+                thumbnailFile: thumbnail,
+            });
 
             reader.onload = () => {
                 this.selectedThumbnail = reader.result;
@@ -37,6 +60,10 @@ export class ExerciseCreatorComponent {
     }
 
     removeThumbnail() {
+        this.exerciseForm.patchValue({
+            thumbnailFile: null,
+        });
+
         this.selectedThumbnail = null;
     }
 
@@ -44,6 +71,10 @@ export class ExerciseCreatorComponent {
         if (this.videoInput.nativeElement.files) {
             const video = this.videoInput.nativeElement.files[0];
             const reader = new FileReader();
+
+            this.exerciseForm.patchValue({
+                videoFile: video,
+            });
 
             reader.onload = () => {
                 this.selectedVideo = reader.result;
@@ -54,6 +85,18 @@ export class ExerciseCreatorComponent {
     }
 
     removeVideo() {
+        this.exerciseForm.patchValue({
+            videoFile: null,
+        });
+
         this.selectedVideo = null;
+    }
+
+    setEquipmentId($event: string | null) {
+        this.exerciseForm.patchValue({
+            equipmentId: $event,
+        });
+
+        console.log(this.exerciseForm.value);
     }
 }
