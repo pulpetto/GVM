7b4f05593065e72da2d78f6dbbb3bb34bfc97a21
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-26 11:00:03 +0200

    delete initial data

--- src/app/shared/exercise-creator/instruction-steps/instruction-steps.component.ts
+++ src/app/shared/exercise-creator/instruction-steps/instruction-steps.component.ts
@@ -11,46 +11,7 @@ import { Step } from '../../../interfaces/step';
     styleUrl: './instruction-steps.component.css',
 })
 export class InstructionStepsComponent {
-    steps: Step[] = [
-        {
-            name: 'Setup',
-            subSteps: [
-                'Stand with your feet hip-width apart and the bar over your mid-foot.',
-                'Bend down and grip the bar just outside your knees.',
-                'Keep your back straight and chest up.',
-            ],
-        },
-        {
-            name: 'Lift',
-            subSteps: [
-                'Push through your heels and stand up, lifting the bar.',
-                'Keep the bar close to your body.',
-                'Stand straight with the bar at thigh level.',
-            ],
-        },
-        {
-            name: 'Lockout',
-            subSteps: [
-                'Stand tall with your hips and knees fully extended.',
-                'Keep your shoulders back.',
-            ],
-        },
-        {
-            name: 'Descent',
-            subSteps: [
-                'Push your hips back and bend your knees to lower the bar.',
-                'Keep the bar close to your body.',
-                'Lower the bar to the ground.',
-            ],
-        },
-        {
-            name: 'Reset',
-            subSteps: [
-                'Ensure your setup is correct before lifting again.',
-                'Repeat the steps for additional repetitions.',
-            ],
-        },
-    ];
+    steps: Step[] = [];
 
     newStepNameEditorVisibility: boolean = false;
     newStepName: string = '';
