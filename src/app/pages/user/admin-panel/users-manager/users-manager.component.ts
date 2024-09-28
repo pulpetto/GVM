a0597143ca54728459d1fd2f50bf9af846300815
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-28 10:58:18 +0200

    add component

---
+++ src/app/pages/user/admin-panel/users-manager/users-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-users-manager',
+    standalone: true,
+    imports: [],
+    templateUrl: './users-manager.component.html',
+    styleUrl: './users-manager.component.css',
+})
+export class UsersManagerComponent {}
