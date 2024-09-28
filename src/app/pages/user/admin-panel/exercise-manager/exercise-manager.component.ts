46943c9a3103300926eb3c4a44f30622247f55ed
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-28 09:23:58 +0200

    get & display exercises list

--- src/app/pages/user/admin-panel/exercise-manager/exercise-manager.component.ts
+++ src/app/pages/user/admin-panel/exercise-manager/exercise-manager.component.ts
@@ -1,14 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AdminService } from '../../../../services/admin.service';
 import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { Exercise } from '../../../../interfaces/exercise';
 
 @Component({
     selector: 'app-exercise-manager',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, CommonModule],
     templateUrl: './exercise-manager.component.html',
     styleUrl: './exercise-manager.component.css',
 })
-export class ExerciseManagerComponent {
+export class ExerciseManagerComponent implements OnInit {
     adminService = inject(AdminService);
+
+    exercises$!: Observable<Exercise[]>;
+
+    ngOnInit() {
+        this.exercises$ = this.adminService.getExercises();
+    }
 }
