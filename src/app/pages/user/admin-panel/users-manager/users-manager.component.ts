cda7c62a2b61b76d4ababbbca8dbd1260ccdea7e
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-09-29 07:00:25 +0200

    get & display users

--- src/app/pages/user/admin-panel/users-manager/users-manager.component.ts
+++ src/app/pages/user/admin-panel/users-manager/users-manager.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AdminService } from '../../../../services/admin.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { User } from '../../../../interfaces/user';
 
 @Component({
     selector: 'app-users-manager',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './users-manager.component.html',
     styleUrl: './users-manager.component.css',
 })
-export class UsersManagerComponent {}
+export class UsersManagerComponent implements OnInit {
+    adminService = inject(AdminService);
+
+    users$!: Observable<User[]>;
+
+    ngOnInit() {
+        this.users$ = this.adminService.getUsers$();
+    }
+}
