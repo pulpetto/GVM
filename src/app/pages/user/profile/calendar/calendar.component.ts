ee2ac1e2f36b29e9701ed73204a6738dec89b331
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-10-05 04:54:26 +0200

    animate modal

--- src/app/pages/user/profile/calendar/calendar.component.ts
+++ src/app/pages/user/profile/calendar/calendar.component.ts
@@ -1,8 +1,6 @@
 import {
     Component,
     computed,
-    ElementRef,
-    HostListener,
     Inject,
     Renderer2,
     Signal,
@@ -13,6 +11,18 @@ import { CommonModule } from '@angular/common';
 import { DOCUMENT } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DateTime, Info, Interval } from 'luxon';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const visibleModal = { top: '25%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const visibleBtnFixed = { bottom: '0' };
+const hiddenBtnFixed = { bottom: '-100%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-calendar',
@@ -20,6 +30,38 @@ import { DateTime, Info, Interval } from 'luxon';
     templateUrl: './calendar.component.html',
     styleUrl: './calendar.component.css',
     imports: [CommonModule, RouterModule],
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose3', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
+            ]),
+        ]),
+    ],
 })
 export class CalendarComponent {
     today: Signal<DateTime> = signal(
@@ -63,29 +105,21 @@ export class CalendarComponent {
     goToCurrentMonth() {
         this.firstDayOfActiveMonth.set(this.today().startOf('month'));
     }
+
     // ---------- ---------- ---------- ---------- ----------
 
-    isOpen: boolean = false;
+    monthChangeModalVisibility: boolean = false;
 
     constructor(
-        private elementRef: ElementRef,
         private renderer: Renderer2,
         @Inject(DOCUMENT) private document: Document
     ) {}
 
-    @HostListener('document:click', ['$event'])
-    clickout(event: Event) {
-        if (!this.elementRef.nativeElement.contains(event.target)) {
-            this.renderer.removeClass(this.document.body, 'overflow-y-hidden');
-            this.isOpen = false;
-        }
-    }
-
     toggleModalVisibility() {
-        this.isOpen = !this.isOpen;
+        this.monthChangeModalVisibility = !this.monthChangeModalVisibility;
 
         // prettier-ignore
-        this.isOpen
+        this.monthChangeModalVisibility
             ? this.renderer.addClass(this.document.body, 'overflow-y-hidden')
             : this.renderer.removeClass(
                 this.document.body,
