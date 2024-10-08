07862427f6ed60c14a67d1958adf4ea7cb966938
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-10-08 05:19:03 +0200

    add component

---
+++ src/app/shared/workoutViews/workout-template-editor/start-date/start-date.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-start-date',
+    standalone: true,
+    imports: [],
+    templateUrl: './start-date.component.html',
+    styleUrl: './start-date.component.css',
+})
+export class StartDateComponent {}
