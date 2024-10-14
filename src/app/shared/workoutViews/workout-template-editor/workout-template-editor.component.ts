72cea3998431b19559517507db9aa5e966a97177
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-10-14 11:33:53 +0200

    add more colors for supersets

--- src/app/shared/workoutViews/workout-template-editor/workout-template-editor.component.ts
+++ src/app/shared/workoutViews/workout-template-editor/workout-template-editor.component.ts
@@ -177,20 +177,20 @@ export class WorkoutTemplateEditorComponent implements OnInit {
     colors: string[] = [
         'bg-red-500',
         'bg-orange-500',
-        // 'bg-amber-500',
+        'bg-amber-500',
         'bg-yellow-500',
-        // 'bg-lime-500',
+        'bg-lime-500',
         'bg-green-500',
-        // 'bg-teal-500',
-        // 'bg-cyan-500',
+        'bg-teal-500',
+        'bg-cyan-500',
         'bg-sky-500',
         'bg-blue-500',
-        // 'bg-indigo-500',
+        'bg-indigo-500',
         'bg-violet-500',
-        // 'bg-purple-500',
+        'bg-purple-500',
         'bg-fuchsia-500',
-        // 'bg-pink-500',
-        // 'bg-rose-500',
+        'bg-pink-500',
+        'bg-rose-500',
     ];
 
     initalizeSupersetModal($event: (string | number)[]) {
