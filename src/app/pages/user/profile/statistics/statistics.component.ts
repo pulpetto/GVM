0ea0656930ca701767825094b50629bdf6df2596
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-10-22 20:05:21 +0200

    add common module

--- src/app/pages/user/profile/statistics/statistics.component.ts
+++ src/app/pages/user/profile/statistics/statistics.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-statistics',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './statistics.component.html',
     styleUrl: './statistics.component.css',
 })
