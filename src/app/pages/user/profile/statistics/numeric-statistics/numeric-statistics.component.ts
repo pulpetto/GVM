22a5d85f9946a99d4e445463edcaabb1149ff460
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-02 14:26:25 +0100

    add markup

--- src/app/pages/user/profile/statistics/numeric-statistics/numeric-statistics.component.ts
+++ src/app/pages/user/profile/statistics/numeric-statistics/numeric-statistics.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-numeric-statistics',
-  standalone: true,
-  imports: [],
-  templateUrl: './numeric-statistics.component.html',
-  styleUrl: './numeric-statistics.component.css'
+    selector: 'app-numeric-statistics',
+    standalone: true,
+    imports: [],
+    templateUrl: './numeric-statistics.component.html',
+    styleUrl: './numeric-statistics.component.css',
 })
-export class NumericStatisticsComponent {
-
-}
+export class NumericStatisticsComponent {}
