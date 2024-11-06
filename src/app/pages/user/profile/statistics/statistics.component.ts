815583bd96cda9db940598dc4090e09a14851c3a
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-06 11:22:20 +0100

    get workouts data & react for data type or period change

--- src/app/pages/user/profile/statistics/statistics.component.ts
+++ src/app/pages/user/profile/statistics/statistics.component.ts
@@ -87,53 +87,216 @@ export class StatisticsComponent implements OnInit {
     chartLabels!: string[];
     chartMaxLabelsLimit!: number;
 
+    workoutsAmount: number = 0;
     setsAmount: number = 0;
     repsAmount: number = 0;
     timeSpentUnix: number = 0;
     volume: number = 0;
 
+    oldestFetchedDate!: DateTime;
+
     ngOnInit() {
         const now = DateTime.now();
-        const startDate = now.minus({ months: 1 });
+        this.oldestFetchedDate = now.minus({ months: 1 });
 
         this.userService.user$
             .pipe(
                 filter((user) => !!user),
-                switchMap(() =>
-                    this.userService.getDoneWorkoutsStartingFromUnix(
-                        startDate.toUnixInteger()
-                    )
-                )
+                switchMap(() => this.userService.getAllDoneWorkouts())
             )
             .subscribe((data) => {
                 this.workouts = data;
 
-                this.workouts.forEach((workout) => {
-                    this.timeSpentUnix += workout.duration;
+                this.activeDataType = this.dataTypes[0];
+                this.activePeriod = this.periods[0];
 
-                    workout.exercises.forEach((exercise) =>
-                        exercise.sets.forEach((set) => {
-                            if (set.isDone) {
-                                this.setsAmount++;
-                                this.repsAmount += +set.reps;
-                                this.volume += +set.reps * +set.weight;
-                            }
-                        })
-                    );
-                });
+                this.changeDataType(this.activeDataType);
             });
-
-        this.activeDataType = this.dataTypes[0];
-        this.activePeriod = this.periods[0];
-    }
-
-    changePeriod(period: string) {
-        this.activePeriod = period;
     }
 
     changeDataType(dataType: string) {
         this.activeDataType = dataType;
 
+        if (dataType === 'Rep ranges types') {
+            this.chartLabels = ['Strength', 'Hypertrophy', 'Endurance'];
+            this.chartData = [0, 0, 0];
+
+            this.workouts.forEach((workout) =>
+                workout.exercises.forEach((exercise) =>
+                    exercise.sets.forEach((set) => {
+                        if (set.isDone) {
+                            if (set.reps <= 5) {
+                                this.chartData[0]++;
+                            }
+
+                            if (set.reps >= 6 && set.reps <= 20) {
+                                this.chartData[1]++;
+                            }
+
+                            if (set.reps > 20) {
+                                this.chartData[2]++;
+                            }
+                        }
+                    })
+                )
+            );
+        }
+
+        if (dataType === 'Training intensity') {
+            this.chartLabels = ['Failure', 'RPE 8-9', 'RPE 6-7'];
+            this.chartData = [0, 0, 0];
+
+            this.workouts.forEach((workout) =>
+                workout.exercises.forEach((exercise) =>
+                    exercise.sets.forEach((set) => {
+                        if (set.isDone) {
+                            if (set.rpe === 'F') {
+                                this.chartData[0]++;
+                            }
+
+                            if (set.rpe === 8 || set.rpe === 9) {
+                                this.chartData[1]++;
+                            }
+
+                            if (set.rpe === 7 || set.rpe === 6) {
+                                this.chartData[2]++;
+                            }
+                        }
+                    })
+                )
+            );
+        }
+
+        if (dataType === 'Workouts duration') {
+            this.workouts.forEach((workout) => {
+                this.chartData.push(workout.duration);
+            });
+        }
+
+        if (dataType === 'Workouts reps') {
+            this.workouts.forEach((workout) => {
+                workout.exercises.forEach((exercise) =>
+                    exercise.sets.forEach((set) => {
+                        if (set.isDone) {
+                            this.chartData.push(+set.reps);
+                        }
+                    })
+                );
+            });
+        }
+
+        if (dataType === 'Workouts volume') {
+            this.workouts.forEach((workout) => {
+                workout.exercises.forEach((exercise) =>
+                    exercise.sets.forEach((set) => {
+                        if (set.isDone) {
+                            this.chartData.push(+set.reps * +set.weight);
+                        }
+                    })
+                );
+            });
+        }
+
+        this.changePeriod('1M');
+    }
+
+    changePeriod(period: string) {
+        this.activePeriod = period;
+
+        const now = DateTime.now();
+        let startDate: DateTime | null;
+
+        if (period === '1M') startDate = now.minus({ months: 1 });
+
+        if (period === '3M') startDate = now.minus({ months: 3 });
+
+        if (period === '6M') startDate = now.minus({ months: 6 });
+
+        if (period === '1Y') startDate = now.minus({ years: 1 });
+
+        if (period === 'All') startDate = null;
+
+        const workouts: WorkoutDone[] = this.workouts.filter(
+            (workout) =>
+                workout.dateStart >=
+                (startDate ? startDate!.toUnixInteger() : 0)
+        );
+
+        if (
+            this.activeDataType === 'Workouts duration' ||
+            this.activeDataType === 'Workouts reps' ||
+            this.activeDataType === 'Workouts volume'
+        )
+            this.setLineChartLabels(workouts);
+
+        this.setNumericStats(workouts);
+
         this.dataTypeModalVisibility = false;
+    }
+
+    setLineChartLabels(workouts: WorkoutDone[]) {
+        this.chartLabels = [];
+
+        workouts.forEach((workout) => {
+            if (this.activePeriod === '1M') {
+                this.chartMaxLabelsLimit = 5;
+
+                this.chartLabels.push(
+                    DateTime.fromMillis(workout.dateStart * 1000).toFormat(
+                        'MMM d'
+                    )
+                );
+            }
+
+            if (this.activePeriod === '3M') {
+                this.chartMaxLabelsLimit = 5;
+
+                this.chartLabels.push(
+                    DateTime.fromMillis(workout.dateStart * 1000).setLocale(
+                        'en'
+                    ).monthLong!
+                );
+            }
+
+            if (this.activePeriod === '6M' || this.activePeriod === '1Y') {
+                this.chartMaxLabelsLimit = 5;
+
+                this.chartLabels.push(
+                    DateTime.fromMillis(workout.dateStart * 1000).setLocale(
+                        'en'
+                    ).monthShort!
+                );
+            }
+
+            if (this.activePeriod === 'All') {
+                this.chartMaxLabelsLimit = 5;
+
+                this.chartLabels.push(
+                    DateTime.fromMillis(workout.dateStart * 1000).year + ''
+                );
+            }
+        });
+    }
+
+    setNumericStats(workouts: WorkoutDone[]) {
+        this.workoutsAmount = workouts.length;
+        this.timeSpentUnix = 0;
+        this.setsAmount = 0;
+        this.repsAmount = 0;
+        this.volume = 0;
+
+        workouts.forEach((workout) => {
+            this.timeSpentUnix += workout.duration;
+
+            workout.exercises.forEach((exercise) =>
+                exercise.sets.forEach((set) => {
+                    if (set.isDone) {
+                        this.setsAmount++;
+                        this.repsAmount += +set.reps;
+                        this.volume += +set.reps * +set.weight;
+                    }
+                })
+            );
+        });
     }
 }
