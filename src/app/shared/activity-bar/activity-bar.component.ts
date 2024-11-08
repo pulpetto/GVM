bff59244b70e01d16a84eea1e18d5befc6bc38c4
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-08 15:36:03 +0100

    add input & content projection for customization

--- src/app/shared/activity-bar/activity-bar.component.ts
+++ src/app/shared/activity-bar/activity-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-activity-bar',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
     templateUrl: './activity-bar.component.html',
     styleUrl: './activity-bar.component.css',
 })
-export class ActivityBarComponent {}
+export class ActivityBarComponent {
+    @Input({ required: true }) title!: string;
+}
