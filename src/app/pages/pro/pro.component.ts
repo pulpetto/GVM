65652709719720b4079ff6b4e71c6ed4d01a6656
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-08 15:36:37 +0100

    add component

---
+++ src/app/pages/pro/pro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-pro',
+    standalone: true,
+    imports: [],
+    templateUrl: './pro.component.html',
+    styleUrl: './pro.component.css',
+})
+export class ProComponent {}
