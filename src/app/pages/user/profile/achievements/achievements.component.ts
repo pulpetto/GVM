95f98f11f28cd8809fa41b516e81d2e41e94f7cf
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-09 04:59:47 +0100

    add activity bar

--- src/app/pages/user/profile/achievements/achievements.component.ts
+++ src/app/pages/user/profile/achievements/achievements.component.ts
@@ -4,11 +4,18 @@ import { DataService } from '../../../../services/data.service';
 import { Observable } from 'rxjs';
 import { Achievement } from '../../../../interfaces/achievement';
 import { CommonModule } from '@angular/common';
+import { ActivityBarComponent } from '../../../../shared/activity-bar/activity-bar.component';
+import { PreviousRouteButtonComponent } from '../../../../shared/previous-route-button/previous-route-button.component';
 
 @Component({
     selector: 'app-achievements',
     standalone: true,
-    imports: [AchievementComponent, CommonModule],
+    imports: [
+        AchievementComponent,
+        CommonModule,
+        ActivityBarComponent,
+        PreviousRouteButtonComponent,
+    ],
     templateUrl: './achievements.component.html',
     styleUrl: './achievements.component.css',
 })
