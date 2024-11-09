719364d8ec8fb9ee0b993e51af02a3d650751797
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-09 10:14:49 +0100

    add markup & design

--- src/app/pages/pro/pro.component.ts
+++ src/app/pages/pro/pro.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
     templateUrl: './pro.component.html',
     styleUrl: './pro.component.css',
 })
-export class ProComponent {}
+export class ProComponent {
+    freeFeatures: string[] = [
+        'Workout tracking',
+        'All exercises with instructions',
+        'Workout history with calendar',
+    ];
+
+    proFeatures: string[] = [
+        'Unlimited workout templates',
+        'Unlimited custom exercises',
+        'Unlimited exercises per workout',
+        'Advanced statistics',
+        'Goals tracking',
+        'Achievements tracking',
+        'Full statistics history',
+        'Premium profile badge',
+        'Support for GVM development',
+    ];
+}
