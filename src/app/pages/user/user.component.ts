28faac15619cf3073a90c4a97c85a72a417e832d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-11 08:09:39 +0100

    display navbar accordingly to service state data

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    inject,
+} from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { NavbarVisibilityService } from '../../services/navbar-visibility.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-user',
     standalone: true,
     templateUrl: './user.component.html',
     styleUrl: './user.component.css',
-    imports: [NavbarComponent, RouterOutlet],
+    imports: [NavbarComponent, RouterOutlet, CommonModule],
 })
-export class UserComponent {}
+export class UserComponent implements AfterViewInit {
+    navbarVisibilityService = inject(NavbarVisibilityService);
+    cdr = inject(ChangeDetectorRef);
+
+    navbarVisibility$!: Observable<boolean>;
+
+    ngAfterViewInit() {
+        this.navbarVisibility$ =
+            this.navbarVisibilityService.visibility.asObservable();
+
+        this.cdr.detectChanges();
+    }
+}
