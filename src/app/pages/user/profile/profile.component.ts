d2b4ad1681b4987427ea8ce0b64e06355b51a050
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-18 17:43:09 +0100

    add pfp fullscreen preview

--- src/app/pages/user/profile/profile.component.ts
+++ src/app/pages/user/profile/profile.component.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { UserService } from '../../../services/user.service';
 import { RouterModule } from '@angular/router';
 import { ActivityBarComponent } from '../../../shared/activity-bar/activity-bar.component';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const timing = '0.3s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-profile',
@@ -16,10 +22,24 @@ import { ActivityBarComponent } from '../../../shared/activity-bar/activity-bar.
         RouterModule,
         ActivityBarComponent,
     ],
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+    ],
 })
 export class ProfileComponent {
     userService = inject(UserService);
 
     userRole = computed(() => this.userService.getUser()?.role);
     proPlan = computed(() => this.userService.getUser()?.pro);
+
+    pfpFullView: boolean = true;
 }
