d2a86ba1ce0ca462f727a3076d4e14b9557013f7
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-19 08:48:55 +0100

    redesign page

--- src/app/pages/user/profile/settings/settings.component.ts
+++ src/app/pages/user/profile/settings/settings.component.ts
@@ -1,13 +1,4 @@
-import {
-    AfterViewInit,
-    Component,
-    ElementRef,
-    inject,
-    OnDestroy,
-    QueryList,
-    Renderer2,
-    ViewChildren,
-} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserService } from '../../../../services/user.service';
 import { RouterModule } from '@angular/router';
 import { ActivityBarComponent } from '../../../../shared/activity-bar/activity-bar.component';
@@ -19,55 +10,10 @@ import { ActivityBarComponent } from '../../../../shared/activity-bar/activity-b
     templateUrl: './settings.component.html',
     styleUrl: './settings.component.css',
 })
-export class SettingsComponent implements AfterViewInit, OnDestroy {
+export class SettingsComponent {
     userService = inject(UserService);
 
     logout() {
         this.userService.logoutUser();
-    }
-
-    @ViewChildren('stickyElements') stickyElements!: QueryList<ElementRef>;
-
-    constructor(private renderer: Renderer2) {}
-
-    ngAfterViewInit() {
-        this.checkStickyPositions();
-        window.addEventListener('scroll', this.checkStickyPositions.bind(this));
-    }
-
-    checkStickyPositions() {
-        this.stickyElements.forEach((stickyElement: ElementRef) => {
-            const stickyOffset =
-                stickyElement.nativeElement.getBoundingClientRect().top;
-
-            if (stickyOffset <= 0) {
-                this.renderer.addClass(
-                    stickyElement.nativeElement,
-                    'text-center'
-                );
-                this.renderer.addClass(stickyElement.nativeElement, '-m-4');
-                this.renderer.addClass(
-                    stickyElement.nativeElement,
-                    'rounded-t-none'
-                );
-            } else {
-                this.renderer.removeClass(
-                    stickyElement.nativeElement,
-                    'text-center'
-                );
-                this.renderer.removeClass(stickyElement.nativeElement, '-m-4');
-                this.renderer.removeClass(
-                    stickyElement.nativeElement,
-                    'rounded-t-none'
-                );
-            }
-        });
-    }
-
-    ngOnDestroy() {
-        window.removeEventListener(
-            'scroll',
-            this.checkStickyPositions.bind(this)
-        );
     }
 }
