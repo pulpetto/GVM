fe8e414fddb75940d128447a03354bf11bea9c59
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-20 05:04:44 +0100

    add markup

--- src/app/pages/user/profile/settings/account/account.component.ts
+++ src/app/pages/user/profile/settings/account/account.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ActivityBarComponent } from '../../../../../shared/activity-bar/activity-bar.component';
+import { PreviousRouteButtonComponent } from '../../../../../shared/previous-route-button/previous-route-button.component';
 
 @Component({
     selector: 'app-account',
     standalone: true,
-    imports: [],
+    imports: [ActivityBarComponent, PreviousRouteButtonComponent],
     templateUrl: './account.component.html',
     styleUrl: './account.component.css',
 })
