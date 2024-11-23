77b956cc1120f1df65f2c94456de9d36cdcf1daa
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-23 08:02:00 +0100

    add modal for theming

--- src/app/pages/user/profile/settings/settings.component.ts
+++ src/app/pages/user/profile/settings/settings.component.ts
@@ -2,16 +2,63 @@ import { Component, inject } from '@angular/core';
 import { UserService } from '../../../../services/user.service';
 import { RouterModule } from '@angular/router';
 import { ActivityBarComponent } from '../../../../shared/activity-bar/activity-bar.component';
+import { CommonModule } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const visibleModal = { top: '50%' };
+const hiddenModal = { top: '100%' };
+
+const visibleBg = { opacity: '100%' };
+const hiddenBg = { opacity: '0%' };
+
+const visibleBtnFixed = { bottom: '0' };
+const hiddenBtnFixed = { bottom: '-100%' };
+
+const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 
 @Component({
     selector: 'app-settings',
     standalone: true,
-    imports: [RouterModule, ActivityBarComponent],
+    imports: [RouterModule, ActivityBarComponent, CommonModule],
     templateUrl: './settings.component.html',
     styleUrl: './settings.component.css',
+    animations: [
+        trigger('openClose', [
+            transition(':enter', [
+                style(hiddenModal),
+                animate(timing, style(visibleModal)),
+            ]),
+            transition(':leave', [
+                style(visibleModal),
+                animate(timing, style(hiddenModal)),
+            ]),
+        ]),
+        trigger('openClose2', [
+            transition(':enter', [
+                style(hiddenBg),
+                animate(timing, style(visibleBg)),
+            ]),
+            transition(':leave', [
+                style(visibleBg),
+                animate(timing, style(hiddenBg)),
+            ]),
+        ]),
+        trigger('openClose3', [
+            transition(':enter', [
+                style(hiddenBtnFixed),
+                animate(timing, style(visibleBtnFixed)),
+            ]),
+            transition(':leave', [
+                style(visibleBtnFixed),
+                animate(timing, style(hiddenBtnFixed)),
+            ]),
+        ]),
+    ],
 })
 export class SettingsComponent {
     userService = inject(UserService);
+
+    themeModalVisibility: boolean = true;
 
     logout() {
         this.userService.logoutUser();
