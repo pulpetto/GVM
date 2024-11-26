3c8ffc0ca94bdf28d7824b247f57923d60c9204a
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-11-26 04:56:52 +0100

    calculate estimated 1rm and display it

--- src/app/pages/user/profile/goals/goals-creator/goals-creator.component.ts
+++ src/app/pages/user/profile/goals/goals-creator/goals-creator.component.ts
@@ -1,8 +1,12 @@
 import { animate, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExerciseSelectorComponent } from '../goalsCreator/exercise-selector/exercise-selector.component';
 import { ExercisePreview } from '../../../../../interfaces/exercise-preview';
+import { UserService } from '../../../../../services/user.service';
+import { FormsModule } from '@angular/forms';
+import { NgxMaskDirective } from 'ngx-mask';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 const visibleModal = { top: '50%' };
 const hiddenModal = { top: '100%' };
@@ -18,7 +22,12 @@ const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 @Component({
     selector: 'app-goals-creator',
     standalone: true,
-    imports: [CommonModule, ExerciseSelectorComponent],
+    imports: [
+        CommonModule,
+        ExerciseSelectorComponent,
+        FormsModule,
+        NgxMaskDirective,
+    ],
     templateUrl: './goals-creator.component.html',
     styleUrl: './goals-creator.component.css',
     animations: [
@@ -55,10 +64,15 @@ const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
     ],
 })
 export class GoalsCreatorComponent {
+    userService = inject(UserService);
+    destroyRef = inject(DestroyRef);
+
     newGoalModalVisibility: boolean = false;
     exerciseSelectorModalVisibility: boolean = false;
 
     selectedExercise: ExercisePreview | null = null;
+    estimated1rm!: number;
+    goal1rm!: string;
 
     closeNewGoalModal() {
         this.selectedExercise = null;
@@ -67,6 +81,30 @@ export class GoalsCreatorComponent {
 
     selectExercise($event: ExercisePreview) {
         this.selectedExercise = $event;
+        this.userService
+            .getDoneWorkoutsByExerciseId($event.id)
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((workouts) => {
+                workouts.forEach((workout) => {
+                    const exercise = workout.exercises.find(
+                        (exercise) => exercise.exerciseId === $event.id
+                    );
+
+                    let heaviestWeight = 0;
+                    let reps = 0;
+
+                    exercise!.sets.forEach((set) => {
+                        if (+set.weight > heaviestWeight) {
+                            heaviestWeight = +set.weight;
+                            reps = +set.reps;
+                        }
+                    });
+
+                    this.estimated1rm = Math.round(
+                        heaviestWeight * (1 + 0.0333 * reps)
+                    );
+                });
+            });
         this.newGoalModalVisibility = true;
     }
 }
