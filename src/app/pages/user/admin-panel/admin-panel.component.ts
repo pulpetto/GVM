d8cea779a5f69319e0528da034e32ce99226cefc
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-12-01 08:50:39 +0100

    add activity bar

--- src/app/pages/user/admin-panel/admin-panel.component.ts
+++ src/app/pages/user/admin-panel/admin-panel.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ActivityBarComponent } from '../../../shared/activity-bar/activity-bar.component';
 
 @Component({
     selector: 'app-admin-panel',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, ActivityBarComponent],
     templateUrl: './admin-panel.component.html',
     styleUrl: './admin-panel.component.css',
 })
