cb43ee0977ea77c0056be3ff30c3137e6ae51596
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-12-01 12:46:58 +0100

    add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -86,6 +86,15 @@ export const routes: Routes = [
                     ).then((component) => component.EquipmentManagerComponent),
             },
             {
+                path: 'admin/achievements',
+                loadComponent: () =>
+                    import(
+                        './pages/user/admin-panel/achievements-manager/achievements-manager.component'
+                    ).then(
+                        (component) => component.AchievementsManagerComponent
+                    ),
+            },
+            {
                 path: 'profile',
                 loadComponent: () =>
                     import('./pages/user/profile/profile.component').then(
