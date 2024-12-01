573df0a3bec7cb04eac53c57f7fc0a3bfcd32710
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-12-01 19:55:31 +0100

    add activity bar

--- src/app/pages/user/admin-panel/achievements-manager/achievements-manager.component.ts
+++ src/app/pages/user/admin-panel/achievements-manager/achievements-manager.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ActivityBarComponent } from '../../../../shared/activity-bar/activity-bar.component';
+import { PreviousRouteButtonComponent } from '../../../../shared/previous-route-button/previous-route-button.component';
 
 @Component({
     selector: 'app-achievements-manager',
     standalone: true,
-    imports: [],
+    imports: [ActivityBarComponent, PreviousRouteButtonComponent],
     templateUrl: './achievements-manager.component.html',
     styleUrl: './achievements-manager.component.css',
 })
