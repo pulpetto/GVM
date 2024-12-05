9a17954ba39f38e5266c55c37d0b9c34dec9287a
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-12-05 16:28:59 +0100

    get & display achievements with progress

--- src/app/pages/user/profile/achievements/achievements.component.ts
+++ src/app/pages/user/profile/achievements/achievements.component.ts
@@ -1,12 +1,29 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AchievementComponent } from '../../../../shared/achievement/achievement.component';
 import { DataService } from '../../../../services/data.service';
-import { Observable } from 'rxjs';
+import { filter, Observable, switchMap } from 'rxjs';
 import { Achievement } from '../../../../interfaces/achievement';
 import { CommonModule } from '@angular/common';
 import { ActivityBarComponent } from '../../../../shared/activity-bar/activity-bar.component';
 import { PreviousRouteButtonComponent } from '../../../../shared/previous-route-button/previous-route-button.component';
 import { UserService } from '../../../../services/user.service';
+
+interface AchievementsTypes {
+    [key: string]: number;
+    totalWorkoutDuration: number;
+    workoutsAmount: number; //
+    totalWorkoutsVolume: number; //
+    totalWorkoutsReps: number; //
+    totalWorkoutsSets: number; //
+    workoutsOver2Hours: number;
+    workoutsOver20kVolume: number;
+    workoutsOver100Reps: number;
+    workoutsOver20Sets: number;
+    estimatedSquat1rm: number;
+    estimatedBench1rm: number;
+    estimatedDeadlift1rm: number; //
+    powerliftingTotal: number; //
+}
 
 @Component({
     selector: 'app-achievements',
@@ -26,7 +43,118 @@ export class AchievementsComponent implements OnInit {
 
     achievements$!: Observable<Achievement[]>;
 
+    achievementsValues: AchievementsTypes = {
+        totalWorkoutDuration: 0,
+        workoutsAmount: 0,
+        totalWorkoutsVolume: 0,
+        totalWorkoutsReps: 0,
+        totalWorkoutsSets: 0,
+        workoutsOver2Hours: 0,
+        workoutsOver20kVolume: 0,
+        workoutsOver100Reps: 0,
+        workoutsOver20Sets: 0,
+        estimatedSquat1rm: 0,
+        estimatedBench1rm: 0,
+        estimatedDeadlift1rm: 0,
+        powerliftingTotal: 0,
+    };
+
+    loading: boolean = true;
+
     ngOnInit() {
         this.achievements$ = this.dataService.getAchievements$();
+
+        this.userService.user$
+            .pipe(
+                filter((user) => !!user),
+                switchMap(() => this.userService.getAllDoneWorkouts())
+            )
+            .subscribe((workouts) => {
+                workouts.forEach((workout) => {
+                    let repsInWorkout = 0;
+                    let setsInWorkout = 0;
+
+                    this.achievementsValues.totalWorkoutDuration +=
+                        workout.duration;
+
+                    workout.exercises.forEach((exercise) => {
+                        exercise.sets.forEach((set) => {
+                            if (set.isDone) {
+                                this.achievementsValues.totalWorkoutsVolume +=
+                                    set.reps * set.weight;
+
+                                this.achievementsValues.totalWorkoutsReps +=
+                                    +set.reps;
+                                repsInWorkout += set.reps;
+
+                                this.achievementsValues.totalWorkoutsSets++;
+                                setsInWorkout++;
+                            }
+                        });
+
+                        if (
+                            exercise.exerciseId === '59wzXlkwNom13oL3OiNM' ||
+                            exercise.exerciseId === 'SdMfU5CvUwSBdl9jgp3M' ||
+                            exercise.exerciseId === '0Jd1ahlkGgsNzexbdzVU'
+                        ) {
+                            let heaviestWeight = 0;
+                            let reps = 0;
+
+                            exercise!.sets.forEach((set) => {
+                                if (+set.weight > heaviestWeight) {
+                                    heaviestWeight = +set.weight;
+                                    reps = +set.reps;
+                                }
+                            });
+
+                            const oneRm = Math.round(
+                                heaviestWeight * (1 + 0.0333 * reps)
+                            );
+
+                            if (exercise.exerciseId === '59wzXlkwNom13oL3OiNM')
+                                if (
+                                    oneRm >
+                                    this.achievementsValues.estimatedSquat1rm
+                                )
+                                    this.achievementsValues.estimatedSquat1rm =
+                                        oneRm;
+
+                            if (exercise.exerciseId === 'SdMfU5CvUwSBdl9jgp3M')
+                                if (
+                                    oneRm >
+                                    this.achievementsValues.estimatedBench1rm
+                                )
+                                    this.achievementsValues.estimatedBench1rm =
+                                        oneRm;
+
+                            if (exercise.exerciseId === '0Jd1ahlkGgsNzexbdzVU')
+                                if (
+                                    oneRm >
+                                    this.achievementsValues.estimatedDeadlift1rm
+                                )
+                                    this.achievementsValues.estimatedDeadlift1rm =
+                                        oneRm;
+                        }
+                    });
+
+                    if (workout.duration >= 7200)
+                        this.achievementsValues.workoutsOver2Hours++;
+                    if (workout.volume >= 20000)
+                        this.achievementsValues.workoutsOver20kVolume++;
+                    if (repsInWorkout > 100)
+                        this.achievementsValues.workoutsOver100Reps++;
+                    if (setsInWorkout > 20)
+                        this.achievementsValues.workoutsOver20Sets++;
+                });
+
+                this.achievementsValues.powerliftingTotal =
+                    this.achievementsValues.estimatedSquat1rm +
+                    this.achievementsValues.estimatedBench1rm +
+                    this.achievementsValues.estimatedDeadlift1rm;
+
+                this.achievementsValues.workoutsAmount = workouts.length;
+
+                this.loading = false;
+            });
     }
 }
--- src/app/shared/achievement/achievement.component.ts
+++ src/app/shared/achievement/achievement.component.ts
@@ -12,11 +12,28 @@ import { NumberSeparatorPipe } from '../../pipes/number-separator.pipe';
 })
 export class AchievementComponent implements OnInit {
     @Input({ required: true }) achievement!: Achievement;
+    @Input({ required: true }) progress!: number;
 
     percentageProgress!: number;
     activeTier: number = 0;
 
     ngOnInit() {
-        this.percentageProgress = 55;
+        if (this.progress === 0) {
+            this.activeTier = 0;
+            this.percentageProgress = 0;
+        } else if (this.progress >= this.achievement.tiers[3].to) {
+            this.activeTier = 3;
+            this.percentageProgress = 100;
+        } else {
+            this.achievement.tiers.forEach((tier, i) => {
+                if (this.progress >= tier.from && this.progress < tier.to)
+                    this.activeTier = i;
+            });
+
+            this.percentageProgress = Math.round(
+                (this.progress / this.achievement.tiers[this.activeTier].to) *
+                    100
+            );
+        }
     }
 }
