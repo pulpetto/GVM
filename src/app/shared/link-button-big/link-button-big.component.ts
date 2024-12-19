187d7ddec71420be8cba4a7b20ecabb27181dcfb
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-12-19 13:41:57 +0100

    add markup & inputs

--- src/app/shared/link-button-big/link-button-big.component.ts
+++ src/app/shared/link-button-big/link-button-big.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-link-button-big',
     standalone: true,
-    imports: [],
+    imports: [RouterModule],
     templateUrl: './link-button-big.component.html',
     styleUrl: './link-button-big.component.css',
 })
-export class LinkButtonBigComponent {}
+export class LinkButtonBigComponent {
+    @Input({ required: true }) name!: string;
+    @Input({ required: true }) link!: string;
+}
