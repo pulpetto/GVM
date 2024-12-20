09e6d9e643d2c00164fb193129b534e88acadfb3
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-12-20 09:19:35 +0100

    swap buttons

--- src/app/pages/user/admin-panel/admin-panel.component.ts
+++ src/app/pages/user/admin-panel/admin-panel.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ActivityBarComponent } from '../../../shared/activity-bar/activity-bar.component';
+import { LinkButtonBigComponent } from '../../../shared/link-button-big/link-button-big.component';
 
 @Component({
     selector: 'app-admin-panel',
     standalone: true,
-    imports: [RouterModule, ActivityBarComponent],
+    imports: [RouterModule, ActivityBarComponent, LinkButtonBigComponent],
     templateUrl: './admin-panel.component.html',
     styleUrl: './admin-panel.component.css',
 })
