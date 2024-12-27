cb65e7d8539a4b7cbfc4a71157e53244fdbf166c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-12-27 18:25:42 +0100

    remove unused import

--- src/app/pages/user/profile/profile.component.ts
+++ src/app/pages/user/profile/profile.component.ts
@@ -4,7 +4,6 @@ import { UserService } from '../../../services/user.service';
 import { RouterModule } from '@angular/router';
 import { ActivityBarComponent } from '../../../shared/activity-bar/activity-bar.component';
 import { animate, style, transition, trigger } from '@angular/animations';
-import { LinkButtonBigComponent } from '../../../shared/link-button-big/link-button-big.component';
 import { LinkBgIconComponent } from '../../../shared/link-bg-icon/link-bg-icon.component';
 
 const visibleBg = { opacity: '100%' };
@@ -21,7 +20,6 @@ const timing = '0.3s cubic-bezier(0.4, 0, 0.2, 1)';
         CommonModule,
         RouterModule,
         ActivityBarComponent,
-        LinkButtonBigComponent,
         LinkBgIconComponent,
     ],
     animations: [
