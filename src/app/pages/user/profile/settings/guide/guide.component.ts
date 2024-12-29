e6a8916bc22ce56f3406bddda1fa34728346b061
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-12-29 06:37:16 +0100

    add content to guide

--- src/app/pages/user/profile/settings/guide/guide.component.ts
+++ src/app/pages/user/profile/settings/guide/guide.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ActivityBarComponent } from '../../../../../shared/activity-bar/activity-bar.component';
+import { PreviousRouteButtonComponent } from '../../../../../shared/previous-route-button/previous-route-button.component';
 
 @Component({
     selector: 'app-guide',
     standalone: true,
-    imports: [],
+    imports: [ActivityBarComponent, PreviousRouteButtonComponent],
     templateUrl: './guide.component.html',
     styleUrl: './guide.component.css',
 })
