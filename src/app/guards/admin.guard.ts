d694bb9fc7bccf30746a189c70df19965bbbafe7
pulpetto <116948697+pulpetto@users.noreply.github.com>
2024-12-30 08:09:02 +0100

    fix guard

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,16 +1,24 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs';
 
 export const adminGuard: CanActivateFn = (route, state) => {
     const userService = inject(UserService);
     const router = inject(Router);
 
-    const user = userService.getUser();
-    if (user?.role === 'admin') {
-        return true;
-    } else {
-        router.navigate(['/unauthorized']);
-        return false;
-    }
+    const user = userService.getUser;
+
+    return toObservable(user).pipe(
+        filter((user) => !!user),
+        map((user) => {
+            if (user?.role === 'admin') {
+                return true;
+            } else {
+                router.navigate(['/unauthorized']);
+                return false;
+            }
+        })
+    );
 };
