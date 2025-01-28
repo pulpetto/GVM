665c0faca04d30208827a4cdb0401bda92d0500e
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-01-28 15:26:44 +0100

    add activity bar

--- src/app/pages/user/profile/assistant/assistant.component.ts
+++ src/app/pages/user/profile/assistant/assistant.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ActivityBarComponent } from '../../../../shared/activity-bar/activity-bar.component';
+import { PreviousRouteButtonComponent } from '../../../../shared/previous-route-button/previous-route-button.component';
 
 @Component({
     selector: 'app-assistant',
     standalone: true,
-    imports: [],
+    imports: [ActivityBarComponent, PreviousRouteButtonComponent],
     templateUrl: './assistant.component.html',
     styleUrl: './assistant.component.css',
 })
