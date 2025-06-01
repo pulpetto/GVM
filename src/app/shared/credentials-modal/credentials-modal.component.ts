d4efca577be01924b5980b8dbfad6b804614592d
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-01 06:47:21 +0200

    change initial visibility

--- src/app/shared/credentials-modal/credentials-modal.component.ts
+++ src/app/shared/credentials-modal/credentials-modal.component.ts
@@ -56,7 +56,7 @@ const timing = '0.5s cubic-bezier(0.4, 0, 0.2, 1)';
 export class CredentialsModalComponent {
     userService = inject(UserService);
 
-    modalVisibility: boolean = true;
+    modalVisibility: boolean = false;
 
     loginUser(email: string, password: string) {
         this.userService.loginUser(email, password);
