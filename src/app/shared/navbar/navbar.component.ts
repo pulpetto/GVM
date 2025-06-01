1edb23acd8ec453f33954c41626611f46ad65af3
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-01 13:28:36 +0200

    make navbar a sidebar from xl tailwind size

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { UserService } from '../../services/user.service';
@@ -15,4 +15,15 @@ export class NavbarComponent {
     userService = inject(UserService);
 
     userRole = computed(() => this.userService.getUser()?.role);
+
+    private isXl = window.innerWidth >= 1280;
+
+    @HostListener('window:resize')
+    onResize() {
+        this.isXl = window.innerWidth >= 1280;
+    }
+
+    isXlScreen() {
+        return this.isXl;
+    }
 }
